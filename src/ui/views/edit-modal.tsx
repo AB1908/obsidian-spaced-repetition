71bdd39bc5cb620e27161cadafe97b2d7a313cf9
AB1908 <14124383+AB1908@users.noreply.github.com>
2022-10-06 15:06:49 +0530

    refactor: rewrite modal to use jsx

--- src/ui/views/edit-modal.tsx
+++ src/ui/views/edit-modal.tsx
@@ -1,5 +1,8 @@
-import {App, ButtonComponent, Modal, TextAreaComponent} from "obsidian";
+import { App, ButtonComponent, Modal, TextAreaComponent } from "obsidian";
 import SRPlugin from "src/main";
+import React from "react";
+import { render } from "react-dom";
+import { createRoot } from "react-dom/client";
 
 // from https://github.com/chhoumann/quickadd/blob/bce0b4cdac44b867854d6233796e3406dfd163c6/src/gui/GenericInputPrompt/GenericInputPrompt.ts#L5
 export class FlashcardEditModal extends Modal {
@@ -12,6 +15,7 @@ export class FlashcardEditModal extends Modal {
     private didSubmit: boolean = false;
     private inputComponent: TextAreaComponent;
     private readonly modalText: string;
+    contentRoot: any;
 
     public static Prompt(app: App, plugin: SRPlugin, placeholder: string): Promise<string> {
         const newPromptModal = new FlashcardEditModal(app, plugin, placeholder);
@@ -36,59 +40,88 @@ export class FlashcardEditModal extends Modal {
         this.contentEl.empty();
         this.titleEl.textContent = "Edit Flashcard";
         this.modalEl.addClass("sr-input-modal");
+        this.contentRoot = createRoot(this.contentEl);
+        this.contentRoot.render(
+            (
+            // <div className="modal-content">
+                <div className="sr-input-area">
+                    <textarea spellCheck="false"
+                        style={{ width: "100%" }}
+                        defaultValue={this.modalText}
+                        onKeyDown={(e) => this.submitEnterCallback(e)}
+                        onChange={(event) => { this.input = event.target.value; }}
+                    />
+                    <div
+                        style={{
+                            display: "flex",
+                            flexDirection: "row-reverse",
+                            justifyContent: "flex-start",
+                            marginTop: "1rem"
+                        }}
+                    >
+                        <button className="mod-cta" style={{ marginRight: 0 }} onClick={(e) => this.submitClickCallback(e as unknown as MouseEvent)}>
+                            Ok
+                        </button>
+                        <button onClick={(e) => this.cancelClickCallback(e as unknown as MouseEvent)}>Cancel</button>
+                    </div>
+                </div>
+            // </div>
+            )
+        )
 
-        const mainContentContainer: HTMLDivElement = this.contentEl.createDiv();
-        mainContentContainer.addClass("sr-input-area");
-        this.inputComponent = this.createInputField(mainContentContainer, this.modalText);
-        this.createButtonBar(mainContentContainer);
+        // const mainContentContainer: HTMLDivElement = this.contentEl.createDiv();
+        // mainContentContainer.addClass("sr-input-area");
+        // this.inputComponent = this.createInputField(mainContentContainer, this.modalText);
+        // this.createButtonBar(mainContentContainer);
     }
 
-    private createButton(container: HTMLElement, text: string, callback: (evt: MouseEvent) => any) {
-        const btn = new ButtonComponent(container);
-        btn.setButtonText(text).onClick(callback);
+    // private createButton(container: HTMLElement, text: string, callback: (evt: MouseEvent) => any) {
+    //     const btn = new ButtonComponent(container);
+    //     btn.setButtonText(text).onClick(callback);
 
-        return btn;
-    }
+    //     return btn;
+    // }
 
-    private createButtonBar(mainContentContainer: HTMLDivElement) {
-        const buttonBarContainer: HTMLDivElement = mainContentContainer.createDiv();
-        this.createButton(
-            buttonBarContainer,
-            "Ok",
-            this.submitClickCallback
-        ).setCta().buttonEl.style.marginRight = "0";
-        this.createButton(buttonBarContainer, "Cancel", this.cancelClickCallback);
+    // private createButtonBar(mainContentContainer: HTMLDivElement) {
+    //     const buttonBarContainer: HTMLDivElement = mainContentContainer.createDiv();
+    //     this.createButton(
+    //         buttonBarContainer,
+    //         "Ok",
+    //         this.submitClickCallback
+    //     ).setCta().buttonEl.style.marginRight = "0";
+    //     this.createButton(buttonBarContainer, "Cancel", this.cancelClickCallback);
 
-        buttonBarContainer.style.display = "flex";
-        buttonBarContainer.style.flexDirection = "row-reverse";
-        buttonBarContainer.style.justifyContent = "flex-start";
-        buttonBarContainer.style.marginTop = "1rem";
-    }
+    //     buttonBarContainer.style.display = "flex";
+    //     buttonBarContainer.style.flexDirection = "row-reverse";
+    //     buttonBarContainer.style.justifyContent = "flex-start";
+    //     buttonBarContainer.style.marginTop = "1rem";
+    // }
 
-    protected createInputField(container: HTMLElement, value: string) {
-        const textComponent = new TextAreaComponent(container);
+    // protected createInputField(container: HTMLElement, value: string) {
+    //     const textComponent = new TextAreaComponent(container);
 
-        textComponent.inputEl.style.width = "100%";
-        textComponent
-            .setValue(value ?? "")
-            .onChange((value) => (this.input = value))
-            .inputEl.addEventListener("keydown", this.submitEnterCallback);
+    //     textComponent.inputEl.style.width = "100%";
+    //     textComponent
+    //         .setValue(value ?? "")
+    //         .onChange((value) => (this.input = value))
+    //         .inputEl.addEventListener("keydown", this.submitEnterCallback);
 
-        return textComponent;
-    }
+    //     return textComponent;
+    // }
 
-    private submitClickCallback = (evt: MouseEvent) => this.submit();
-    private cancelClickCallback = (evt: MouseEvent) => this.cancel();
+    private submitClickCallback = (evt) => this.submit(evt.target.value);
+    private cancelClickCallback = (evt) => this.cancel();
 
     private submitEnterCallback = (evt: KeyboardEvent) => {
         if ((evt.ctrlKey || evt.metaKey) && evt.key === "Enter") {
             evt.preventDefault();
-            this.submit();
+            this.submit(evt.target.value);
         }
     };
 
-    private submit() {
+    private submit(submittedText: string) {
         this.didSubmit = true;
+        this.input = submittedText;
 
         this.close();
     }
@@ -100,13 +133,13 @@ export class FlashcardEditModal extends Modal {
     onOpen() {
         super.onOpen();
 
-        this.inputComponent.inputEl.focus();
+        // this.inputComponent.inputEl.focus();
     }
 
     onClose() {
         super.onClose();
         this.resolveInput();
-        this.removeInputListener();
+        // this.removeInputListener();
     }
 
     private resolveInput() {
@@ -114,7 +147,7 @@ export class FlashcardEditModal extends Modal {
         else this.resolvePromise(this.input);
     }
 
-    private removeInputListener() {
-        this.inputComponent.inputEl.removeEventListener("keydown", this.submitEnterCallback);
-    }
+    // private removeInputListener() {
+    //     this.inputComponent.inputEl.removeEventListener("keydown", this.submitEnterCallback);
+    // }
 }
