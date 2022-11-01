df86dc66c1cf3ce749e2c9660267843ed7d8ffb2
AB1908 <14124383+AB1908@users.noreply.github.com>
2022-11-01 16:51:02 +0530

    refactor: move vault.modify back to processCardResponse()

--- src/flashcard-modal.tsx
+++ src/flashcard-modal.tsx
@@ -269,16 +269,16 @@ export class FlashcardModal extends Modal {
             let fileText: string = await this.app.vault.read(currentCard.note);
             const replacementRegex = new RegExp(escapeRegexString(cardText), "gm");
             fileText = fileText.replace(replacementRegex, () => newCardText);
-            await this.buryAndJumpToNextCard(currentDeck, index, currentCard, fileText, this.plugin.data.settings.burySiblingCards);
+            await this.buryCardAndSiblings(currentDeck, index, currentCard, fileText, this.plugin.data.settings.burySiblingCards);
+            await this.plugin.app.vault.modify(currentCard.note, fileText);
         }
     }
 
-    private async buryAndJumpToNextCard(currentDeck: Deck, index: number, currentCard: Card, fileText: string, shouldBurySiblings: boolean) {
+    private async buryCardAndSiblings(currentDeck: Deck, index: number, currentCard: Card, fileText: string, shouldBurySiblings: boolean) {
         currentDeck.deleteFlashcardAtIndex(index, currentCard.isDue);
         if (shouldBurySiblings) {
-            burySiblingCards(true, currentCard, currentDeck);
+            await burySiblingCards(true, currentCard, currentDeck);
         }
-        await this.plugin.app.vault.modify(currentCard.note, fileText);
     }
 
     private processCrammedCards(response: ReviewResponse, currentDeck: Deck, index: number, currentCard: Card) {
