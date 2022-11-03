62384eeb264660690a98d5580afc4b28573b8735
AB1908 <14124383+AB1908@users.noreply.github.com>
2022-11-03 18:58:21 +0530

    refactor: convert CollapseIcon to FC

--- src/ui/views/deck.tsx
+++ src/ui/views/deck.tsx
@@ -32,33 +32,27 @@ function InnerTreeItem(props: SubDeckProps) {
     );
 }
 
-class CollapseIcon extends Component<StateChanger> {
-    constructor(props: StateChanger) {
-        super(props);
-    }
-
-    render(): ReactNode {
-        return (
-            <div
-                className="tree-item-icon collapse-icon"
-                onClick={(e) => this.props.handleTriangleClick(e)}
+function CollapseIcon(props: StateChanger) {
+    return (
+        <div
+            className="tree-item-icon collapse-icon"
+            onClick={(e) => props.handleTriangleClick(e)}
+        >
+            <svg
+                viewBox="0 0 100 100"
+                width={10}
+                height={10}
+                className="right-triangle"
+                style={props.isDeckTreeOpen ? {} : { transform: "rotate(-90deg)" }}
             >
-                <svg
-                    viewBox="0 0 100 100"
-                    width={10}
-                    height={10}
-                    className="right-triangle"
-                    style={this.props.isDeckTreeOpen ? {} : { transform: "rotate(-90deg)" }}
-                >
-                    <path
-                        fill="currentColor"
-                        stroke="currentColor"
-                        d="M94.9,20.8c-1.4-2.5-4.1-4.1-7.1-4.1H12.2c-3,0-5.7,1.6-7.1,4.1c-1.3,2.4-1.2,5.2,0.2,7.6L43.1,88c1.5,2.3,4,3.7,6.9,3.7 s5.4-1.4,6.9-3.7l37.8-59.6C96.1,26,96.2,23.2,94.9,20.8L94.9,20.8z"
-                    />
-                </svg>
-            </div>
-        );
-    }
+                <path
+                    fill="currentColor"
+                    stroke="currentColor"
+                    d="M94.9,20.8c-1.4-2.5-4.1-4.1-7.1-4.1H12.2c-3,0-5.7,1.6-7.1,4.1c-1.3,2.4-1.2,5.2,0.2,7.6L43.1,88c1.5,2.3,4,3.7,6.9,3.7 s5.4-1.4,6.9-3.7l37.8-59.6C96.1,26,96.2,23.2,94.9,20.8L94.9,20.8z"
+                />
+            </svg>
+        </div>
+    );
 }
 
 function CollapsibleDeckTreeEntry(props: SubDeckProps) {
@@ -78,7 +72,7 @@ function CollapsibleDeckTreeEntry(props: SubDeckProps) {
                 >
                     {/* tree-item-self*/}
                     <CollapseIcon
-                        {...{isDeckTreeOpen}}
+                        {...{ isDeckTreeOpen }}
                         handleTriangleClick={(e: MouseEvent) => handleTriangleClick(e)}
                     />
                     <InnerTreeItem
