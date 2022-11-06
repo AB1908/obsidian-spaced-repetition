bf3c47f17e895dececbf214773bb8b236508ead2
AB1908 <14124383+AB1908@users.noreply.github.com>
2022-11-06 08:22:59 +0530

    refactor: move scheduled text methods into separate file

---
+++ src/sched-utils.ts
@@ -0,0 +1,70 @@
+import {Card} from "src/scheduling";
+import {LEGACY_SCHEDULING_EXTRACTOR, MULTI_SCHEDULING_EXTRACTOR} from "src/constants";
+import {PluginData} from "src/main";
+
+export function removeSchedTextFromCard(cardText: string) {
+    return cardText.replace(/<!--SR:.+-->/gm, "");
+}
+
+function generateSchedInfoString(scheduling: RegExpMatchArray[]) {
+    let schedInfo = "<!--SR:";
+    for (let i = 0; i < scheduling.length; i++) {
+        schedInfo += `!${scheduling[i][1]},${scheduling[i][2]},${scheduling[i][3]}`;
+    }
+    schedInfo += "-->";
+    return schedInfo;
+}
+
+function generateCardTextWithSchedInfo(cardText: string, scheduling: RegExpMatchArray[]) {
+    cardText = removeSchedTextFromCard(cardText);
+    let schedInfo = generateSchedInfoString(scheduling);
+    return cardText + schedInfo;
+}
+
+function generateSeparator(cardText: string, isCardCommentOnSameLine: boolean) {
+    let sep: string = isCardCommentOnSameLine ? " " : "\n";
+    // Override separator if last block is a codeblock
+    if (cardText.endsWith("```") && sep !== "\n") {
+        sep = "\n";
+    }
+    return sep;
+}
+
+export function generateSchedulingArray(cardText: string, dueString: string, interval: number, ease: number, currentCard: Card) {
+    console.group("input")
+    console.log(arguments)
+    console.groupEnd()
+    let scheduling: RegExpMatchArray[] = [
+        ...cardText.matchAll(MULTI_SCHEDULING_EXTRACTOR),
+    ];
+    if (scheduling.length === 0) {
+        scheduling = [...cardText.matchAll(LEGACY_SCHEDULING_EXTRACTOR)];
+    }
+
+    const currCardSched: RegExpMatchArray = ["0", dueString, interval.toString(), ease.toString()];
+    if (currentCard.isDue) {
+        scheduling[currentCard.siblingIdx] = currCardSched;
+    } else {
+        scheduling.push(currCardSched);
+    }
+    console.group("output");
+    console.log(scheduling);
+    console.groupEnd();
+    return scheduling;
+}
+
+function regenerateCardTextWithSchedInfo(cardText: string, sep: string, dueString: string, interval: number, ease: number, currentCard: Card) {
+    // adding info to the card for the first time
+    if (cardText.lastIndexOf("<!--SR:") === -1) {
+        return cardText + sep + `<!--SR:!${dueString},${interval},${ease}-->`;
+    } else {
+        let scheduling = generateSchedulingArray(cardText, dueString, interval, ease, currentCard);
+        return generateCardTextWithSchedInfo(cardText, scheduling);
+    }
+}
+
+export function updateCardText(currentCard: Card, data: PluginData, dueString: string, interval: number, ease: number, isCardCommentOnSameLine: boolean = data.settings.cardCommentOnSameLine) {
+    let cardText = currentCard.cardText;
+    const sep = generateSeparator(cardText, isCardCommentOnSameLine);
+    return regenerateCardTextWithSchedInfo(cardText, sep, dueString, interval, ease, currentCard);
+}
