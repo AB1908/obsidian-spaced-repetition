930059a55c890efef7a695857a28c795999815fe
AB1908 <14124383+AB1908@users.noreply.github.com>
2022-12-09 14:04:36 +0530

    refactor(DeckBuilder): move sibling generators to Sibling.ts

--- src/DeckBuilder.ts
+++ src/DeckBuilder.ts
@@ -9,6 +9,13 @@ import {SRSettings} from "./settings";
 import {Stats} from "./stats-modal";
 import {cyrb53, escapeRegexString} from "./utils";
 import {Card} from "src/Card";
+import {
+    generateClozeSiblingMatches,
+    multiLineBasicSiblingMatches,
+    multiLineReversedSiblingMatches,
+    singleLineBasicSiblingMatches,
+    singleLineReversedSiblingMatches
+} from "src/Sibling";
 
 //TODO: Also include decks that don't have due flashcards
 export async function sync(syncLock: boolean, setSyncLock: Function, data: PluginData): Promise<Deck> {
@@ -128,44 +135,6 @@ export async function sync(syncLock: boolean, setSyncLock: Function, data: Plugi
     setSyncLock(false);
     console.log(deckTree);
     return deckTree;
-}
-
-export function generateClozeSiblingMatches(settings: SRSettings, cardText: string): [string, string][] {
-    return findSiblingMatches(generateSiblings(settings, cardText), cardText);
-}
-
-function singleLineBasicSiblingMatches(cardText: string, settings: SRSettings): [string, string] {
-    let idx = cardText.indexOf(settings.singlelineCardSeparator);
-    return [
-        cardText.substring(0, idx),
-        cardText.substring(idx + settings.singlelineCardSeparator.length),
-    ];
-}
-
-function singleLineReversedSiblingMatches(cardText: string, settings: SRSettings): [string, string][] {
-    let idx = cardText.indexOf(settings.singlelineReversedCardSeparator);
-    const side1: string = cardText.substring(0, idx),
-        side2: string = cardText.substring(
-            idx + settings.singlelineReversedCardSeparator.length
-        );
-    return [[side1, side2], [side2, side1]];
-}
-
-function multiLineBasicSiblingMatches(cardText: string, settings: SRSettings): [string, string] {
-    let idx = cardText.indexOf("\n" + settings.multilineCardSeparator + "\n");
-    return [
-        cardText.substring(0, idx),
-        cardText.substring(idx + 2 + settings.multilineCardSeparator.length),
-    ];
-}
-
-function multiLineReversedSiblingMatches(cardText: string, settings: SRSettings): [string, string][] {
-    let idx = cardText.indexOf("\n" + settings.multilineReversedCardSeparator + "\n");
-    const side1: string = cardText.substring(0, idx),
-        side2: string = cardText.substring(
-            idx + 2 + settings.multilineReversedCardSeparator.length
-        );
-    return [[side1, side2], [side2, side1]];
 }
 
 function deleteSchedulingDates(cardText: string, siblingMatches: [string, string][], scheduling: RegExpMatchArray[], fileText: string, fileChanged: boolean) {
@@ -299,67 +268,6 @@ async function findFlashcardsInNote(
     }
 
     return 0;
-}
-
-function generateSiblings(settings: SRSettings, cardText: string) {
-    const siblings: RegExpMatchArray[] = [];
-    if (settings.convertHighlightsToClozes) {
-        siblings.push(...cardText.matchAll(/==(.*?)==/gm));
-    }
-    if (settings.convertBoldTextToClozes) {
-        siblings.push(...cardText.matchAll(/\*\*(.*?)\*\*/gm));
-    }
-    if (settings.convertCurlyBracketsToClozes) {
-        siblings.push(...cardText.matchAll(/{{(.*?)}}/gm));
-    }
-    siblings.sort((a, b) => {
-        if (a.index < b.index) {
-            return -1;
-        }
-        if (a.index > b.index) {
-            return 1;
-        }
-        return 0;
-    });
-    return siblings;
-}
-
-export function generateClozeFront(cardText: string, deletionStart: number, deletionEnd: number) {
-    let front =
-        `${cardText.substring(0, deletionStart)}<span style='color:#2196f3'>[...]</span>${cardText.substring(deletionEnd)}`;
-    return front
-        .replace(/==/gm, "")
-        .replace(/\*\*/gm, "")
-        .replace(/{{/gm, "")
-        .replace(/}}/gm, "");
-}
-
-function generateClozeBack(back: string, cardText: string, deletionStart: number, deletionEnd: number) {
-    back =
-        cardText.substring(0, deletionStart) +
-        "<span style='color:#2196f3'>" +
-        cardText.substring(deletionStart, deletionEnd) +
-        "</span>" +
-        cardText.substring(deletionEnd);
-    return back
-        .replace(/==/gm, "")
-        .replace(/\*\*/gm, "")
-        .replace(/{{/gm, "")
-        .replace(/}}/gm, "");
-}
-
-function findSiblingMatches(siblings: RegExpMatchArray[], cardText: string): [string, string][] {
-    let front: string;
-    let back: string;
-    let matches: [string, string][] = [];
-    for (const m of siblings) {
-        const deletionStart: number = m.index,
-            deletionEnd: number = deletionStart + m[0].length;
-        front = generateClozeFront(cardText, deletionStart, deletionEnd);
-        back = generateClozeBack(back, cardText, deletionStart, deletionEnd);
-        matches.push([front, back]);
-    }
-    return matches;
 }
 
 function getCardContext(cardLine: number, headings: HeadingCache[]): string {
