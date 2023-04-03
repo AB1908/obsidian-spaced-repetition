861a8ef2412ade3c28498517c6a398a74452a207
AB1908 <14124383+AB1908@users.noreply.github.com>
2023-04-03 21:34:33 +0530

    fix: add key instead of id for React lists

--- src/ui/components/book-list.tsx
+++ src/ui/components/book-list.tsx
@@ -1,10 +1,11 @@
 import React from "react";
 
 export function TestComponent({bookList}: {bookList: any}) {
+    //TODO: add logic to emit book object when clicked
     return (<>
             <p>Add flashcards from:</p>
             <ul>
-            {bookList.map((book: any) => (<li id={book.id}>
+            {bookList.map((book: any) => (<li key={book.id}>
                 {book.title}
             </li>))}
             </ul>
