ad87b8faa496f52074624566b0b21756a3ff3b86
AB1908 <14124383+AB1908@users.noreply.github.com>
2023-04-03 21:35:22 +0530

    test: add display test for ChapterList component

--- tests/components.test.tsx
+++ tests/components.test.tsx
@@ -1,13 +1,13 @@
 import React from "react";
 import { render, screen } from "@testing-library/react";
 import { TestComponent } from "src/ui/components/book-list";
+import { ChapterList } from "src/ui/components/chapters-list";
 
-describe("MyComponent", () => {
-    test("renders a div element", () => {
+describe("TestComponent", () => {
+    test("renders a list of books correctly", () => {
         const books = [
             { id: 1, title: "The Great Gatsby", author: "F. Scott Fitzgerald", year: 1925 },
             { id: 2, title: "To Kill a Mockingbird", author: "Harper Lee", year: 1960 },
-            { id: 3, title: "1984", author: "George Orwell", year: 1949 },
         ];
         const { container } = render(<TestComponent bookList={books} />);
         const child = container as HTMLElement;
@@ -24,8 +24,32 @@ describe("MyComponent", () => {
                 <li>
                   To Kill a Mockingbird
                 </li>
+              </ul>
+            </div>
+        `);
+    });
+});
+
+describe("ChapterList", () => {
+    test("should render a list of chapters correctly", () => {
+        const chapters = [
+            { id: 1, title: "Chapter 1" },
+            { id: 2, title: "Chapter 2" },
+        ];
+        const { container } = render(<ChapterList chapterList={chapters} />);
+        const child = container as HTMLElement;
+
+        expect(child).toMatchInlineSnapshot(`
+            <div>
+              <p>
+                Add flashcards from:
+              </p>
+              <ul>
                 <li>
-                  1984
+                  Chapter 1
+                </li>
+                <li>
+                  Chapter 2
                 </li>
               </ul>
             </div>
