26b7ebd9ae0cc61ad778019d9a298a3d1763b44f
AB1908 <14124383+AB1908@users.noreply.github.com>
2023-04-13 16:59:51 +0530

    feat: add correct HTML for rendering a chapter list with coverage

--- src/ui/components/chapters-list.tsx
+++ src/ui/components/chapters-list.tsx
@@ -1,12 +1,30 @@
 import React from "react";
+import {Link} from "react-router-dom";
 
-export function ChapterList({chapterList}: { chapterList: any }) {
+// export function ChapterList({chapterList}: { chapterList: any }) {
+export function ChapterList() {
     //TODO: add logic to emit book object when clicked
-    return (<>
+    const chapterList = [
+
+            { id: 1, title: "Chapter 1", notesWithoutTests: 20, notesWithTests: 11 },
+            { id: 2, title: "Chapter 2", notesWithoutTests: 12, notesWithTests: 15 },
+    ];
+    return (
+        <>
             <p>Add flashcards from:</p>
             <ul>
                 {chapterList.map((chapter: any) => (<li key={chapter.id}>
-                    {chapter.title}
+                    <Link to={""}>
+                        {chapter.title}
+                        <div className={"test-coverage"}>
+                        <span>
+                            {chapter.notesWithoutTests}
+                        </span>
+                            <span>
+                            {chapter.notesWithTests}
+                        </span>
+                        </div>
+                    </Link>
                 </li>))}
             </ul>
         </>
