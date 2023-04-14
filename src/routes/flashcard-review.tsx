12c87956383f9d0a2be32613e0e9cc776479e85e
AB1908 <14124383+AB1908@users.noreply.github.com>
2023-04-14 18:09:57 +0530

    refactor: cleanup unused imports

--- src/routes/flashcard-review.tsx
+++ src/routes/flashcard-review.tsx
@@ -1,11 +1,7 @@
 import {AllCardCounts} from "src/ui/components/card-counts";
 import {Deck} from "src/Deck";
-import React, {useContext, useEffect, useRef, useState} from "react";
-import {DeckTreeView} from "src/ui/views/deck";
-import {sync} from "src/DeckBuilder";
-import {AppContext} from "src/contexts/PluginContext";
-import {Outlet, useNavigate} from "react-router";
-import {Link, NavLink} from "react-router-dom";
+import React from "react";
+import {Link} from "react-router-dom";
 
 export function DeckLandingPage() {
     const deck1 = {dueFlashcardsCount: 10, newFlashcardsCount:20, totalFlashcards: 30, deckName: "Deck1"} as Deck;
