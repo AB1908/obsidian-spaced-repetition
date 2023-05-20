d9dff9f2080ef35fa8f14f063254658e6a6a3dbd
AB1908 <14124383+AB1908@users.noreply.github.com>
2023-05-21 04:19:21 +0530

    feat: add preview page for creation flow if highlights already exist

--- src/ui/components/card-creation.tsx
+++ src/ui/components/card-creation.tsx
@@ -80,6 +80,57 @@ const highlight = highlightsList[0];
     );
 }
 
+export function PreviewExistingFlashcards() {
+    const highlightsList = [
+        {
+            id: 1,
+            highlightContent: "This is a sample highlight",
+            highlightNote: "This is a note for that highlight",
+            flashcards: [
+                {
+                    questionText: "This is a flashcard question that asks about highlight 1",
+                    answerText: "This is the answer to that question"
+                },
+                {
+                    questionText: "Flashcard 2",
+                    answerText: "Answer 2"
+                },
+            ]
+        },
+        {
+            id: 2,
+            highlightContent: "This is a sample highlight but without a note",
+            //TODO: think about whether this should be a null or an empty string on the backend
+            highlightNote: "",
+            flashcards: []
+        },
+    ];
+    const highlight = highlightsList[0];
+    return (
+        <>
+            <NoteAndHighlight/>
+            <div>
+                <p>
+                    Existing questions:
+                </p>
+                <ul>
+                    {highlight.flashcards.map((t,i) => (
+                        <li key={i}>
+                            {t.questionText}
+                        </li>
+                    ))}
+                    <Link to={"/notes/deck/chapters/1/add/regular"}>
+                        <li>
+                            Add new card
+                        </li>
+
+                    </Link>
+                </ul>
+            </div>
+        </>
+    )
+}
+
 export function CreateRegularCard() {
     return (
         <>
@@ -101,11 +152,17 @@ export function CreateRegularCard() {
                </div>
                 <textarea id={"answer"} name={"answer"} className={"sr-answer-input-text"}/>
             </div>
+            {/*
+            TODO: this loops back to the highlights page to choose a highlight
+            */}
             <Link to={""}>
                 <button className={"mod-cta"}>
                     Submit
                 </button>
             </Link>
+            {
+            //     TODO: this loops back to some other page I guess
+            }
             <Link to={""} className={""}>
                 <button>
                     Cancel
