814ab19dcf6108c788890c5f3418bb960a141967
AB1908 <14124383+AB1908@users.noreply.github.com>
2023-05-30 02:10:06 +0530

    refactor: better define dummy chapter data

--- src/ui/components/highlights.tsx
+++ src/ui/components/highlights.tsx
@@ -7,32 +7,35 @@ import {routes} from "src/ui/modals/flashcard-modal";
 // export function ChapterList({chapterList}: { chapterList: any }) {
 export function HighlightsList() {
     //TODO: add logic to emit book object when clicked
-    const highlightsList = [
-        {
-            id: 1,
-            highlightContent: "This is a sample highlight",
-            highlightNote: "This is a note for that highlight",
-            flashcards: [
-                {
-                    questionText: "This is a flashcard question that asks about highlight 1",
-                    answerText: "This is the answer to that question"
-                },
-                {
-                    questionText: "Flashcard 2",
-                    answerText: "Answer 2"
-                },
-            ]
-        },
-        {
-            id: 2,
-            highlightContent: "This is a sample highlight but without a note",
-            //TODO: think about whether this should be a null or an empty string on the backend
-            highlightNote: "",
-            flashcards: []
-        },
-    ];
-    const flashcardList = {
-
+    const chapterData = {
+        id: 1,
+        title: "Chapter 1",
+        notesWithoutTests: 20,
+        notesWithTests: 11,
+        highlights: [
+            {
+                id: 1,
+                highlightContent: "This is a sample highlight",
+                highlightNote: "This is a note for that highlight",
+                flashcards: [
+                    {
+                        questionText: "This is a flashcard question that asks about highlight 1",
+                        answerText: "This is the answer to that question"
+                    },
+                    {
+                        questionText: "Flashcard 2",
+                        answerText: "Answer 2"
+                    },
+                ]
+            },
+            {
+                id: 2,
+                highlightContent: "This is a sample highlight but without a note",
+                //TODO: think about whether this should be a null or an empty string on the backend
+                highlightNote: "",
+                flashcards: []
+            },
+        ],
     }
     const deck1 = {dueFlashcardsCount: 10, newFlashcardsCount:20, totalFlashcards: 30, deckName: "Deck1"} as Deck;
     return (
@@ -41,7 +44,7 @@ export function HighlightsList() {
              This needs to be replaced with the chapter and the highlights done and remaining
              */}
             <h3>
-                {deck1.deckName}
+                {chapterData.title}
             </h3>
             <h4>
                 <AllCardCounts deck={deck1}/>
