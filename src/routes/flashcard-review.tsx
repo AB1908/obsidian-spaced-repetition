f7aaf57d08307f9cd58de608c83591b1ade88106
AB1908 <14124383+AB1908@users.noreply.github.com>
2023-06-04 03:14:05 +0530

    chore: add reminder for fixing counts

--- src/routes/flashcard-review.tsx
+++ src/routes/flashcard-review.tsx
@@ -14,6 +14,7 @@ export function DeckLandingPage() {
             <h4>
                 <AllCardCounts deck={deck1}/>
             </h4>
+            {/*    TODO: Add the untested highlight counts as well?*/}
             <p>
                 <Link to={routes.chapterList}>
                     <button>
