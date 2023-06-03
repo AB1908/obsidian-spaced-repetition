0228f62b9f6be70fdae9d640213838933c18b3b5
AB1908 <14124383+AB1908@users.noreply.github.com>
2023-06-03 22:54:13 +0530

    refactor(ChapterList): use routes const for route path

--- src/routes/flashcard-review.tsx
+++ src/routes/flashcard-review.tsx
@@ -2,6 +2,7 @@ import {AllCardCounts} from "src/ui/components/card-counts";
 import {Deck} from "src/Deck";
 import React from "react";
 import {Link} from "react-router-dom";
+import {routes} from "src/ui/modals/flashcard-modal";
 
 export function DeckLandingPage() {
     const deck1 = {dueFlashcardsCount: 10, newFlashcardsCount:20, totalFlashcards: 30, deckName: "Deck1"} as Deck;
@@ -14,7 +15,7 @@ export function DeckLandingPage() {
                 <AllCardCounts deck={deck1}/>
             </h4>
             <p>
-                <Link to={"/notes/deck/chapters"}>
+                <Link to={routes.chapterList}>
                     <button>
                         Create New Cards
                     </button>
