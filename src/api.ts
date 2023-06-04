24ce162e951dab7ff46331e5ac17205f26c4b536
AB1908 <14124383+AB1908@users.noreply.github.com>
2023-06-05 03:13:32 +0530

    feat: add dummy values for file query API

---
+++ src/api.ts
@@ -0,0 +1,64 @@
+// TODO: decks need to not be coupled directly to the file contents, those should be separate
+// TODO: decide what unique key that a deck
+export const deck = {
+    id: 3,
+    deckName: "Deck1",
+    chapters: [
+        {
+            id: 1,
+            title: "Chapter 1",
+            highlights: [
+                {
+                    id: 1,
+                    highlightContent: "This is a sample highlight",
+                    highlightNote: "This is a note for that highlight",
+                    flashcards: [
+                        {
+                            questionText: "This is a flashcard from question 1",
+                            answerText: "This is the answer to that question"
+                        },
+                        {
+                            questionText: "Flashcard 2 from chapter 1",
+                            answerText: "Answer 2"
+                        },
+                    ]
+                },
+                {
+                    id: 2,
+                    highlightContent: "This is a sample highlight but without a note",
+                    //TODO: think about whether this should be a null or an empty string on the backend
+                    highlightNote: "",
+                    flashcards: []
+                },
+            ],
+        },
+        {
+            id: 2,
+            title: "Chapter 2",
+            highlights: [
+                {
+                    id: 1,
+                    highlightContent: "This is a sample highlight",
+                    highlightNote: "This is a note for that highlight",
+                    flashcards: []
+                },
+                {
+                    id: 2,
+                    highlightContent: "This is a sample highlight but without a note",
+                    //TODO: think about whether this should be a null or an empty string on the backend
+                    highlightNote: "",
+                    flashcards: [
+                        {
+                            questionText: "This is a flashcard question that asks about highlight 2 and is from chapter 2",
+                            answerText: "This is the answer to that question"
+                        },
+                        {
+                            questionText: "Flashcard 2 from chapter 2",
+                            answerText: "Answer 2"
+                        },
+                    ]
+                },
+            ],
+        },
+    ]
+};
