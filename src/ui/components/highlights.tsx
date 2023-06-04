4c8e6a3c1de1179341fe7813f4b1e70bd0033f7b
AB1908 <14124383+AB1908@users.noreply.github.com>
2023-06-05 04:05:01 +0530

    feat: replace hardcoded loader with param based query

--- src/ui/components/highlights.tsx
+++ src/ui/components/highlights.tsx
@@ -2,36 +2,10 @@ import React from "react";
 import {Link} from "react-router-dom";
 import {routes} from "src/ui/modals/flashcard-modal";
 import {useLoaderData} from "react-router";
+import {deck} from "src/api";
 
-export function chapterLoaderData() {
-    const chapterData = {
-        id: 1,
-        title: "Chapter 1",
-        highlights: [
-            {
-                id: 1,
-                highlightContent: "This is a sample highlight",
-                highlightNote: "This is a note for that highlight",
-                flashcards: [
-                    {
-                        questionText: "This is a flashcard question that asks about highlight 1",
-                        answerText: "This is the answer to that question"
-                    },
-                    {
-                        questionText: "Flashcard 2",
-                        answerText: "Answer 2"
-                    },
-                ]
-            },
-            {
-                id: 2,
-                highlightContent: "This is a sample highlight but without a note",
-                //TODO: think about whether this should be a null or an empty string on the backend
-                highlightNote: "",
-                flashcards: []
-            },
-        ],
-    };
+export function chapterLoaderData({params}) {
+    const chapterData = deck.chapters[params.chapterId];
     return chapterData;
 }
 
