5aaeeeb72799b5319efd0b6baac52f8d2daddea7
AB1908 <14124383+AB1908@users.noreply.github.com>
2023-06-05 03:14:41 +0530

    fix: replace hardcoded counts with calculated test counts

--- src/ui/components/chapters-list.tsx
+++ src/ui/components/chapters-list.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import {Link} from "react-router-dom";
 import {routes} from "src/ui/modals/flashcard-modal";
-import {HeaderWithCounts} from "src/ui/components/highlights";
+import {HeaderWithCounts, highlightCountReducer} from "src/ui/components/highlights";
 import {useLoaderData} from "react-router";
 import {deck} from "src/api";
 
@@ -13,19 +13,23 @@ export function countKeyInArray(key: string, array: any) {
     return array.reduce((accumulator: number, currentValue: any) => accumulator + currentValue[key], 0);
 }
 
+// TODO: Fix reduce for counts
 export function ChapterList() {
     const deck1 = useLoaderData();
-    const chapterList = deck1.chapters;
-    const highlightsWithoutTests = countKeyInArray("notesWithoutTests", chapterList);
-    const highlightsWithTests = countKeyInArray("notesWithTests", chapterList);
+    let chapterList = deck1.chapters;
+
+    chapterList = chapterList.map(chapter=>{return {...highlightCountReducer(chapter),...chapter}});
+    console.log(chapterList)
+    const chapterHighlightsWithTests = chapterList.reduce((accumulator, initialValue) => accumulator + initialValue.chapterNotesWithTests,0)
+    const chapterHighlightsWithoutTests = chapterList.reduce((accumulator, initialValue) => accumulator + initialValue.chapterNotesWithoutTests,0)
     return (
         <>
             <h3>
                 {deck1.deckName}
             </h3>
             <HeaderWithCounts
-                withoutCount={highlightsWithoutTests}
-                withCount={highlightsWithTests}
+                withCount={chapterHighlightsWithTests}
+                withoutCount={chapterHighlightsWithoutTests}
             />
             <p>Add flashcards from:</p>
             <ul className={"sr-chapter-tree"}>
@@ -39,11 +43,11 @@ export function ChapterList() {
                                 <span>
                                     <span className={"no-tests tree-item-flair sr-test-counts"}
                                           aria-label={"Cards without tests"}>
-                                        {chapter.notesWithoutTests}
+                                        {chapter.chapterNotesWithTests}
                                     </span>
                                     <span className={"have-tests tree-item-flair sr-test-counts"}
                                           aria-label={"Cards with tests"}>
-                                        {chapter.notesWithTests}
+                                        {chapter.chapterNotesWithoutTests}
                                     </span>
                                 </span>
                             </div>
