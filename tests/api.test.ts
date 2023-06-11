1e79d750c06d6df9b0e8adb8d5be0f3709030fae
AB1908 <14124383+AB1908@users.noreply.github.com>
2023-06-11 20:58:01 +0530

    refactor: change test to expect a flashcard

--- tests/api.test.ts
+++ tests/api.test.ts
@@ -1,5 +1,18 @@
 import {getFlashcardById} from "src/controller";
 
 test("retrieves a flashcard successfully", () => {
-   expect(getFlashcardById("yjlML2s9W")).toBe(false);
+   const flashcard = {
+      "id": "yjlML2s9W",
+      "isDue": true,
+      "questionText": " i-Estel Edain, ú-chebin estel anim.",
+      "answerText": "Onen",
+      "cardText": "==Onen== i-Estel Edain, ==ú-chebin== estel ==anim==.\n<!--SR:!2022-11-14,2,230!2022-11-14,2,210!2022-11-14,2,190-->",
+      "context": "",
+      "cardType": 4,
+      "siblings": [],
+      "interval": 2,
+      "ease": 230,
+      "delayBeforeReview": 17662032301
+   };
+   expect(getFlashcardById("yjlML2s9W")).toStrictEqual(flashcard);
 });
