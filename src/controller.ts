240ac0ca1329c15c61c9979fb58cbedb1555f26f
AB1908 <14124383+AB1908@users.noreply.github.com>
2023-06-11 20:58:24 +0530

    feat: add interface for flashcard

--- src/controller.ts
+++ src/controller.ts
@@ -1,3 +1,17 @@
+export interface Flashcard {
+    id:                string,
+    isDue:             boolean,
+    questionText:      string,
+    answerText:        string,
+    cardText:          string,
+    context:           string,
+    cardType:          number,
+    siblings:          string[],
+    interval:          number,
+    ease:              number,
+    delayBeforeReview: number,
+}
+
 export function getFlashcardById(id: string) {
     return false;
 }
