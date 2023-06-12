93b30cc19395020975a9d4dceebf2b25c3374f43
AB1908 <14124383+AB1908@users.noreply.github.com>
2023-06-12 09:56:05 +0530

    test: refactor test to use a mock this

--- tests/api.test.ts
+++ tests/api.test.ts
@@ -1,19 +1,33 @@
-import {getFlashcardById} from "src/controller";
+import {getFlashcardById, updateFlashcardQuestion} from "src/controller";
+import type {Flashcard} from "src/controller";
 
-test("retrieves a flashcard successfully", () => {
-   const flashcard = {
-      "id": "yjlML2s9W",
-      "isDue": true,
-      "questionText": " i-Estel Edain, ú-chebin estel anim.",
-      "answerText": "Onen",
-      "cardText": "==Onen== i-Estel Edain, ==ú-chebin== estel ==anim==.\n<!--SR:!2022-11-14,2,230!2022-11-14,2,210!2022-11-14,2,190-->",
-      "context": "",
-      "cardType": 4,
-      "siblings": [],
-      "interval": 2,
-      "ease": 230,
-      "delayBeforeReview": 17662032301
-   };
-   expect(getFlashcardById("yjlML2s9W")).toStrictEqual(flashcard);
-   expect(getFlashcardById("aaaa")).toEqual(null);
+const flashcards: Flashcard[] = [{
+    "id": "yjlML2s9W",
+    "isDue": true,
+    "questionText": " i-Estel Edain, ú-chebin estel anim.",
+    "answerText": "Onen",
+    "cardText": "==Onen== i-Estel Edain, ==ú-chebin== estel ==anim==.\n<!--SR:!2022-11-14,2,230!2022-11-14,2,210!2022-11-14,2,190-->",
+    "context": "",
+    "cardType": 4,
+    "siblings": [],
+    "interval": 2,
+    "ease": 230,
+    "delayBeforeReview": 17662032301
+}];
+
+describe('getFlashcardById', () => {
+    let mockThis: { flashcards: Flashcard[] };
+    let boundGet: any;
+
+    beforeEach(() => {
+        mockThis = {
+            flashcards: flashcards
+        };
+        boundGet = getFlashcardById.bind(mockThis);
+    });
+
+    test("retrieves a flashcard successfully", () => {
+        expect(boundGet("yjlML2s9W")).toStrictEqual(flashcards[0]);
+        expect(boundGet("aaaa")).toEqual(null);
+    });
 });
