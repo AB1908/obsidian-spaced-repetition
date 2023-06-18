9cadd6aaabe2fa2008d3716b2132428d91c91409
AB1908 <14124383+AB1908@users.noreply.github.com>
2023-06-18 11:52:03 +0530

    test: add answer update tests

--- tests/api.test.ts
+++ tests/api.test.ts
@@ -1,4 +1,10 @@
-import {createFlashcard, getFlashcardById, updateFlashcardQuestion, deleteFlashcardById} from "src/controller";
+import {
+    createFlashcard,
+    getFlashcardById,
+    updateFlashcardQuestion,
+    deleteFlashcardById,
+    updateFlashcardAnswer
+} from "src/controller";
 import type {Flashcard} from "src/controller";
 import mock = jest.mock;
 
@@ -50,6 +56,33 @@ describe('updateFlashcardQuestion', () => {
 
         expect(boundUpdate(id, updatedQuestion)).toBe(true);
         expect(mockThis.flashcards[0].questionText).toBe(updatedQuestion);
+    });
+
+    test('should return false if the flashcard with the given id does not exist', () => {
+        const nonExistingId = '3';
+        const updatedQuestion = 'What is your age?';
+
+        expect(boundUpdate(nonExistingId, updatedQuestion)).toBe(false);
+    });
+});
+
+describe('updateFlashcardAnswer', () => {
+    let mockThis: { flashcards: Flashcard[] };
+    let boundUpdate: any;
+
+    beforeEach(() => {
+        mockThis = {
+            flashcards: flashcards()
+        };
+        boundUpdate = updateFlashcardAnswer.bind(mockThis);
+    });
+
+    test('should update the questionText of the flashcard with the given id', () => {
+        const updatedAnswer = 'What is your age?';
+        const id = "yjlML2s9W";
+
+        expect(boundUpdate(id, updatedAnswer)).toBe(true);
+        expect(mockThis.flashcards[0].answerText).toBe(updatedAnswer);
     });
 
     test('should return false if the flashcard with the given id does not exist', () => {
