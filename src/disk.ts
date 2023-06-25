6b44fc4ed9e10f75dc2618594bced9bdbac9366b
AB1908 <14124383+AB1908@users.noreply.github.com>
2023-06-26 01:38:18 +0530

    feat: add function stub for updating cards

--- src/disk.ts
+++ src/disk.ts
@@ -1,11 +1,15 @@
 import {TFile} from "obsidian";
 
-export async function writeCardToDisk(file: TFile, cardOnDisk: string) {
-    await app.vault.append(file, cardOnDisk);
+export async function writeCardToDisk(file: TFile, text: string) {
+    await app.vault.append(file, text);
 }
 
 export function getTFileForPath(path: string) {
     const tfile = app.vault.getAbstractFileByPath(path);
-    if (tfile instanceof TFile) return tfile
+    if (tfile instanceof TFile) return tfile;
     else return null;
+}
+
+export function updateCardOnDisk(file: TFile, originalText: string, updatedText: string) {
+    
 }
