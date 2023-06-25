acef8accabae8585dbcd159dcd057f4cbcb04ede
AB1908 <14124383+AB1908@users.noreply.github.com>
2023-06-25 22:12:02 +0530

    test: add checks for metadata parsing

--- tests/deckbuilder.test.ts
+++ tests/deckbuilder.test.ts
@@ -117,3 +117,45 @@ describe("parseAnnotations should parse an annotation with ", () => {
         }]);
     });
 });
+
+describe("metadata parser", () => {
+    test("no metadata", () => {
+        const metadataText: string = "<!--SR:8jfaj0d7!S,2021-08-11,4,270-->";
+
+        // <!--SR:!2022-11-14,2,230!2022-11-14,2,210!2022-11-14,2,190-->
+        expect(parseMetadata("gibberish")).toBe(null);
+        expect(parseMetadata(metadataText)).toEqual({
+            flag: FLAG.SUSPEND,
+            highlightId: "8jfaj0d7",
+            dueDate: "2021-08-11",
+            interval: 4,
+            ease: 270
+        })
+    });
+
+    test("only highlight", () => {
+
+        // <!--SR:!2022-11-14,2,230!2022-11-14,2,210!2022-11-14,2,190-->
+        const metadata: string = "<!--SR:9fn28asl-->";
+        expect(parseMetadata(metadata)).toEqual({
+            highlightId: "9fn28asl",
+            flag: null,
+            dueDate: null,
+            interval: null,
+            ease: null
+        })
+    });
+
+    test("highlight with scheduling info", () => {
+        const metadataText: string = "<!--SR:8jfaj0d7!S,2021-08-11,4,270-->";
+
+        expect(parseMetadata(metadataText)).toEqual({
+            flag: FLAG.SUSPEND,
+            highlightId: "8jfaj0d7",
+            dueDate: "2021-08-11",
+            interval: 4,
+            ease: 270
+        })
+    });
+
+});
