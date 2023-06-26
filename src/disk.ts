9ef9e4970b0c0a32907ec0682d62feecb9438bcf
AB1908 <14124383+AB1908@users.noreply.github.com>
2023-06-26 21:15:10 +0530

    feat: add several methods for reading and writing files and metadata

--- src/disk.ts
+++ src/disk.ts
@@ -1,15 +1,61 @@
 import {TFile} from "obsidian";
 
-export async function writeCardToDisk(file: TFile, text: string) {
-    await app.vault.append(file, text);
+export async function writeCardToDisk(path: string, text: string) {
+    await app.vault.append(getTFileForPath(path), text);
 }
 
-export function getTFileForPath(path: string) {
+export function getTFileForPath(path: string): TFile {
     const tfile = app.vault.getAbstractFileByPath(path);
     if (tfile instanceof TFile) return tfile;
     else return null;
 }
 
-export function updateCardOnDisk(file: TFile, originalText: string, updatedText: string) {
+export function updateCardOnDisk(path: string, originalText: string, updatedText: string) {
     
+}
+
+function setOfHashesWithTags(tag: string) {
+    const findTag = t => t.includes(tag);
+    const hashSet = new Set<string>();
+    const fileHashes = Object.keys(app.metadataCache.metadataCache);
+    for (let hash of fileHashes) {
+        const cachedFileMetadata = app.metadataCache.metadataCache[hash];
+        if (cachedFileMetadata.tags?.find(findTag) || cachedFileMetadata.frontmatter?.tags?.find(findTag)) {
+            hashSet.add(hash)
+        }
+    }
+    return hashSet;
+}
+
+function findFilesWithHashInSet(hashSet: Set<string>) {
+    const filePaths: string[] = [];
+    const fileCacheKeys = Object.keys(app.metadataCache.fileCache);
+    for (let file of fileCacheKeys) {
+        if (app.metadataCache.fileCache[file].hash in hashSet) {
+            filePaths.push(file);
+        }
+    }
+    return filePaths;
+}
+
+export function listOfNotes(tag: string) {
+    const hashSet = setOfHashesWithTags(tag);
+    return findFilesWithHashInSet(hashSet);
+}
+
+export async function getFileContents(path: string) {
+    return await app.vault.read(getTFileForPath(path));
+}
+
+export function getParentFolder(path: string) {
+    // TODO: What if root folder?
+    return getTFileForPath(path)?.parent.path;
+}
+
+export function getHeadersForFile(path: string) {
+    const tfile = getTFileForPath(path);
+    if (tfile === null) {
+        return;
+    }
+    return app.metadataCache.getFileCache(tfile).headings;
 }
