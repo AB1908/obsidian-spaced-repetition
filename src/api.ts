2ea2bfc5f9cbab31df897289f383c5f7b2f261d1
AB1908 <14124383+AB1908@users.noreply.github.com>
2023-06-29 06:20:39 +0530

    chore: rewrite dummy data

--- src/api.ts
+++ src/api.ts
@@ -35,68 +35,55 @@ const flashcards = [
 ]
 
 const highlights = [
-    {
-        id: "d91maa3h",
-        color: "#339122",
-        highlightContent: "Onen i-Estel Edain, ú-chebin estel anim.",
-        highlightNote: "What a beautiful line by Tolkien",
-        flashcards: [
-            "ks991kna",
-        ]
-    },
-    {
-        id: "d91ms7d",
-        color: "#338122",
-        highlightContent: "This is a sample highlight but without a note",
-        //TODO: think about whether this should be a null or an empty string on the backend
-        highlightNote: "",
-        flashcards: []
-    },
-    {
-        id: "9dk1m3jg",
-        color: "#338122",
-        highlightContent: "This is a sample highlight but without a note but also in chapter 1",
-        //TODO: think about whether this should be a null or an empty string on the backend
-        highlightNote: "",
-        flashcards: []
-    }
 ]
 
 export const deck = {
     id: 3,
-    deckName: "Deck1",
-    chapters: [
+    deckName: "Book 1",
+    sections: [
         {
             id: 1,
             title: "Chapter 1",
             highlights: [
                 {
-                    id: 1,
-                    color: "#fda6ce",
-                    highlightContent: "This is a sample highlight from chapter 1",
-                    highlightNote: "Just another note here, testing",
+                    id: "d91maa3h",
+                    color: "#339122",
+                    highlightContent: "Onen i-Estel Edain, ú-chebin estel anim.",
+                    highlightNote: "What a beautiful line by Tolkien",
                     flashcards: [
-                        {
-                            "isDue": true,
-                            "note": null,
-                            "questionText": " i-Estel Edain, ú-chebin estel anim.",
-                            "answerText": "Onen",
-                            "cardText": "==Onen== i-Estel Edain, ==ú-chebin== estel ==anim==.\n<!--SR:!2022-11-14,2,230!2022-11-14,2,210!2022-11-14,2,190-->",
-                            "context": "",
-                            "cardType": 4,
-                            "siblings": [],
-                            "clozeInsertionAt": 0,
-                            "interval": 2,
-                            "ease": 230,
-                            "delayBeforeReview": 17662032301
-                        },
-                        {
-                            questionText: "Flashcard 2 from chapter 1",
-                            answerText: "Answer 2"
-                        },
+                        "ks991kna",
                     ]
                 },
-                "9dk1m3jg",
+                {
+                    id: "d91ms7d",
+                    color: "#338122",
+                    highlightContent: "This is a sample highlight but without a note",
+                    //TODO: think about whether this should be a null or an empty string on the backend
+                    highlightNote: "",
+                    flashcards: []
+                },
+                {
+                    id: 'sadf89u',
+                    title: "Section 1",
+                    highlights: [
+                        {
+                            id: "9dk1m3jg",
+                            color: "#338122",
+                            highlightContent: "This is a sample highlight but without a note but also in chapter 1",
+                            //TODO: think about whether this should be a null or an empty string on the backend
+                            highlightNote: "",
+                            flashcards: []
+                        }
+                    ]
+                },
+                {
+                    id: "9dk1m3jg",
+                    color: "#338122",
+                    highlightContent: "This is a sample highlight but without a note but also in chapter 1",
+                    //TODO: think about whether this should be a null or an empty string on the backend
+                    highlightNote: "",
+                    flashcards: []
+                }
             ],
         },
         {
