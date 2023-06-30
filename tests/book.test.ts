959f02abc7df12340c2d842e7ea5fa0f65e4ec41
AB1908 <14124383+AB1908@users.noreply.github.com>
2023-06-30 17:19:23 +0530

    test: add tests for book helper methods

---
+++ tests/book.test.ts
@@ -0,0 +1,69 @@
+import {AnnotationCount} from "src/data/models/book";
+
+test("recursive counter", () => {
+    let deck = {
+        id: "alksdfj9",
+        name: "Book 1",
+        sections: [
+            {
+                id: "lasdkf8k",
+                title: "Chapter 1",
+                sections: [
+                    {
+                        id: "d91maa3h",
+                        color: "#339122",
+                        highlight: "Onen i-Estel Edain, ú-chebin estel anim.",
+                        note: "What a beautiful line by Tolkien",
+                        flashcards: [
+                        ]
+                    },
+                    {
+                        id: "d91ms7d",
+                        color: "#338122",
+                        highlight: "This is a sample highlight but without a note",
+                        //TODO: think about whether this should be a null or an empty string on the backend
+                        note: "",
+                        flashcards: []
+                    },
+                    {
+                        id: 'sadf89u',
+                        title: "Section 1",
+                        sections: [
+                            {
+                                id: "9dk1m3jg",
+                                color: "#338122",
+                                highlight: "This is a sample highlight but without a note but also in chapter 1",
+                                //TODO: think about whether this should be a null or an empty string on the backend
+                                note: "9dn319d",
+                                flashcards: ["aksdjf"]
+                            }
+                        ]
+                    },
+                    {
+                        id: "9dk1mljg",
+                        color: "#338122",
+                        highlight: "This is a sample highlight but without a note but also in chapter 1",
+                        //TODO: think about whether this should be a null or an empty string on the backend
+                        note: "",
+                        flashcards: []
+                    }
+                ],
+            },
+            {
+                id: "9dn319d",
+                title: "Chapter 2",
+                sections: [
+                ],
+            },
+        ]};
+    // expect(counter(deck, {})).toEqual(1);
+
+    let counter1 = AnnotationCount(deck);
+    expect(counter1).toStrictEqual(
+        {
+            "sadf89u": {with: 1, without: 0},
+            "lasdkf8k": {with: 1, without: 3},
+            "9dn319d": {with: 0, without: 0},
+            "alksdfj9": {with: 1, without: 3},
+        });
+});
