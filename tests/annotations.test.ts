39d3a2dcedf2bc2c4f1829cac2ec9d5feb4fce66
AB1908 <14124383+AB1908@users.noreply.github.com>
2023-07-08 12:06:24 +0530

    refactor: move parseAnnotation tests into annotations.test.ts

---
+++ tests/annotations.test.ts
@@ -0,0 +1,132 @@
+import {parseAnnotations} from "src/data/import/annotations";
+
+describe("parseAnnotations should parse an annotation with", () => {
+    test("a single line highlight", () => {
+        expect(parseAnnotations(`# Heading 1
+
+> [!notes] 93813
+> Onen i estel Edain, u-chebin estel anim.
+> ***
+> 
+`)).toEqual({
+            id: 93813,
+            type: "notes",
+            highlight: "> Onen i estel Edain, u-chebin estel anim.",
+            note: ">"
+        });
+    });
+
+    test("a single line note and single line highlight", () => {
+        expect(parseAnnotations(`# Heading 1
+
+> [!notes] 93813
+> Onen i estel Edain, u-chebin estel anim.
+> ***
+> What a beautiful line by Tolkien
+`)).toEqual({
+            id: 93813,
+            type: "notes",
+            highlight: "> Onen i estel Edain, u-chebin estel anim.",
+            note: "> What a beautiful line by Tolkien"
+        });
+        expect(parseAnnotations(`# Heading 1
+
+> [!notes] 93813
+> Onen i estel Edain, u-chebin estel anim.
+> ***
+> What a beautiful line by Tolkien`)).toEqual({
+            id: 93813,
+            type: "notes",
+            highlight: "> Onen i estel Edain, u-chebin estel anim.",
+            note: "> What a beautiful line by Tolkien"
+        });
+    });
+
+    test("a single line note and multi line highlight", () => {
+        expect(parseAnnotations(`# Heading 1
+
+> [!notes] 93813
+> Onen i estel Edain, u-chebin estel anim.
+> This is another line.
+> ***
+> What a beautiful line by Tolkien`)).toEqual({
+            id: 93813,
+            type: "notes",
+            highlight: "> Onen i estel Edain, u-chebin estel anim.\n> This is another line.",
+            note: "> What a beautiful line by Tolkien"
+        });
+        expect(parseAnnotations(`# Heading 1
+
+> [!notes] 93813
+> Onen i estel Edain, u-chebin estel anim.
+> This is another line.
+> ***
+> What a beautiful line by Tolkien
+`)).toEqual({
+            id: 93813,
+            type: "notes",
+            highlight: "> Onen i estel Edain, u-chebin estel anim.\n> This is another line.",
+            note: "> What a beautiful line by Tolkien"
+        });
+    });
+
+    test("a multi line note and single line highlight", () => {
+
+        expect(parseAnnotations(`# Heading 1
+
+> [!notes] 93813
+> Onen i estel Edain, u-chebin estel anim.
+> ***
+> What a beautiful line by Tolkien
+> This is another line.
+`)).toEqual({
+            id: 93813,
+            type: "notes",
+            highlight: "> Onen i estel Edain, u-chebin estel anim.",
+            note: "> What a beautiful line by Tolkien\n> This is another line."
+        });
+        expect(parseAnnotations(`# Heading 1
+
+> [!notes] 93813
+> Onen i estel Edain, u-chebin estel anim.
+> ***
+> What a beautiful line by Tolkien
+> This is another line.`)).toEqual({
+            id: 93813,
+            type: "notes",
+            highlight: "> Onen i estel Edain, u-chebin estel anim.",
+            note: "> What a beautiful line by Tolkien\n> This is another line."
+        });
+
+    });
+
+    test("a multi line note and multi line highlight", () => {
+        expect(parseAnnotations(`# Heading 1
+
+> [!notes] 93813
+> Onen i estel Edain, u-chebin estel anim.
+> This is another line.
+> ***
+> What a beautiful line by Tolkien
+> This is another line.
+`)).toEqual({
+            id: 93813,
+            type: "notes",
+            highlight: "> Onen i estel Edain, u-chebin estel anim.\n> This is another line.",
+            note: "> What a beautiful line by Tolkien\n> This is another line."
+        });
+        expect(parseAnnotations(`# Heading 1
+
+> [!notes] 93813
+> Onen i estel Edain, u-chebin estel anim.
+> This is another line.
+> ***
+> What a beautiful line by Tolkien
+> This is another line.`)).toEqual({
+            id: 93813,
+            type: "notes",
+            highlight: "> Onen i estel Edain, u-chebin estel anim.\n> This is another line.",
+            note: "> What a beautiful line by Tolkien\n> This is another line."
+        });
+    });
+});
