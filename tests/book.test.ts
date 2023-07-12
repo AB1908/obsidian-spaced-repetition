1b2478b2a7ca1bc0cd22a9387f001ff1bea88735
AB1908 <14124383+AB1908@users.noreply.github.com>
2023-07-12 19:07:06 +0530

    test: add test for bookSection generation

--- tests/book.test.ts
+++ tests/book.test.ts
@@ -1,4 +1,5 @@
-import {AnnotationCount} from "src/data/models/book";
+import { AnnotationCount, bookSections } from "src/data/models/book";
+import { metadataCache, sampleAnnotationMetadata, sampleAnnotationText } from "./disk.test";
 
 test("recursive counter", () => {
     let deck = {
@@ -14,8 +15,7 @@ test("recursive counter", () => {
                         color: "#339122",
                         highlight: "Onen i-Estel Edain, ú-chebin estel anim.",
                         note: "What a beautiful line by Tolkien",
-                        flashcards: [
-                        ]
+                        flashcards: [],
                     },
                     {
                         id: "d91ms7d",
@@ -23,47 +23,150 @@ test("recursive counter", () => {
                         highlight: "This is a sample highlight but without a note",
                         //TODO: think about whether this should be a null or an empty string on the backend
                         note: "",
-                        flashcards: []
+                        flashcards: [],
                     },
                     {
-                        id: 'sadf89u',
+                        id: "sadf89u",
                         title: "Section 1",
                         sections: [
                             {
                                 id: "9dk1m3jg",
                                 color: "#338122",
-                                highlight: "This is a sample highlight but without a note but also in chapter 1",
+                                highlight:
+                                    "This is a sample highlight but without a note but also in chapter 1",
                                 //TODO: think about whether this should be a null or an empty string on the backend
                                 note: "9dn319d",
-                                flashcards: ["aksdjf"]
-                            }
-                        ]
+                                flashcards: ["aksdjf"],
+                            },
+                        ],
                     },
                     {
                         id: "9dk1mljg",
                         color: "#338122",
-                        highlight: "This is a sample highlight but without a note but also in chapter 1",
+                        highlight:
+                            "This is a sample highlight but without a note but also in chapter 1",
                         //TODO: think about whether this should be a null or an empty string on the backend
                         note: "",
-                        flashcards: []
-                    }
+                        flashcards: [],
+                    },
                 ],
             },
             {
                 id: "9dn319d",
                 title: "Chapter 2",
-                sections: [
-                ],
+                sections: [],
             },
-        ]};
+        ],
+    };
     // expect(counter(deck, {})).toEqual(1);
 
     let counter1 = AnnotationCount(deck);
-    expect(counter1).toStrictEqual(
-        {
-            "sadf89u": {with: 1, without: 0},
-            "lasdkf8k": {with: 1, without: 3},
-            "9dn319d": {with: 0, without: 0},
-            "alksdfj9": {with: 1, without: 3},
-        });
+    expect(counter1).toStrictEqual({
+        sadf89u: { with: 1, without: 0 },
+        lasdkf8k: { with: 1, without: 3 },
+        "9dn319d": { with: 0, without: 0 },
+        alksdfj9: { with: 1, without: 3 },
+    });
+});
+
+test("bookSections", () => {
+    expect(bookSections(sampleAnnotationMetadata, sampleAnnotationText)).toMatchInlineSnapshot(`
+        Array [
+          Object {
+            "display": "Header 1",
+            "heading": "Header 1",
+            "level": 1,
+            "position": Object {
+              "end": Object {
+                "col": 10,
+                "line": 10,
+                "offset": 102,
+              },
+              "start": Object {
+                "col": 0,
+                "line": 10,
+                "offset": 92,
+              },
+            },
+          },
+          Object {
+            "highlight": "> Onen i estel Edain, u-chebin estel anim.
+        > This is another line.",
+            "id": 93813,
+            "note": ">",
+            "type": "notes",
+          },
+          Object {
+            "display": "SubHeader 1",
+            "heading": "SubHeader 1",
+            "level": 2,
+            "position": Object {
+              "end": Object {
+                "col": 14,
+                "line": 18,
+                "offset": 212,
+              },
+              "start": Object {
+                "col": 0,
+                "line": 18,
+                "offset": 198,
+              },
+            },
+          },
+          Object {
+            "highlight": "> Onen i estel Edain, u-chebin estel anim.",
+            "id": 93813,
+            "note": "> What a beautiful line by Tolkien",
+            "type": "notes",
+          },
+          Object {
+            "display": "Header 2",
+            "heading": "Header 2",
+            "level": 1,
+            "position": Object {
+              "end": Object {
+                "col": 10,
+                "line": 25,
+                "offset": 326,
+              },
+              "start": Object {
+                "col": 0,
+                "line": 25,
+                "offset": 316,
+              },
+            },
+          },
+          Object {
+            "highlight": "> Onen i estel Edain, u-chebin estel anim.",
+            "id": 93813,
+            "note": "> What a beautiful line by Tolkien
+        > This is another line.",
+            "type": "notes",
+          },
+          Object {
+            "display": "Last header",
+            "heading": "Last header",
+            "level": 1,
+            "position": Object {
+              "end": Object {
+                "col": 13,
+                "line": 33,
+                "offset": 467,
+              },
+              "start": Object {
+                "col": 0,
+                "line": 33,
+                "offset": 454,
+              },
+            },
+          },
+          Object {
+            "highlight": "> Onen i estel Edain, u-chebin estel anim.
+        > This is another line.",
+            "id": 93813,
+            "note": "> What a beautiful line by Tolkien",
+            "type": "notes",
+          },
+        ]
+    `);
 });
