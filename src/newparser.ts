1f5b0216561a8f7358b4ada49bba89c7ed5c9bfb
AB1908 <14124383+AB1908@users.noreply.github.com>
2023-07-12 19:19:01 +0530

    feat: add generateTree implementation

--- src/newparser.ts
+++ src/newparser.ts
@@ -1,5 +1,42 @@
 import {HeadingCache, SectionCache} from "obsidian";
 
+// TODO: refactor
+// TODO: think about heading collisions as there may be multiple chapters with same name
+// TODO: don't need to nest paragraphs I think
+// TODO: fix type
+export function generateTree(sections: any[]) {
+    let i = 0;
+    let prevHeader;
+    while (i<sections.length) {
+        let each = sections[i];
+        if ("heading" in each) {
+            if (!("children" in each)) {
+                each.children = [];
+            }
+        } else {
+            prevHeader = findPreviousHeader(sections, each);
+            if (prevHeader != null) {
+                sections[prevHeader].children.push(each);
+            }
+        }
+        i++;
+    }
+
+    // successively attach lower level headers to higher ones
+    for (let headingLevel = 4; headingLevel > 1; headingLevel--) {
+        let sectionIndex = 0;
+        while (sectionIndex<sections.length) {
+            let each = sections[sectionIndex];
+            if (("heading" in each) && (each.level == headingLevel)) {
+                prevHeader = findPreviousHeader(sections, each);
+                sections[prevHeader].children.push(each);
+            }
+            sectionIndex++;
+        }
+    }
+    return sections.filter(t=>"heading" in t && t.level == 1);
+}
+
 export function findPreviousHeader(sections: (SectionCache|HeadingCache)[], section: SectionCache|HeadingCache) {
     let start = sections.indexOf(section);
     // top level headers don't have a parent
