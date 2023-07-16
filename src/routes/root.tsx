7cc4f25c33ce80a317a3099ec2a2ce1270744ac2
AB1908 <14124383+AB1908@users.noreply.github.com>
2023-07-16 07:37:15 +0530

    refactor: move notes home page to its own file

--- src/routes/root.tsx
+++ src/routes/root.tsx
@@ -1,9 +1,6 @@
-import {AllCardCounts} from "src/ui/components/card-counts";
-import {Deck} from "src/Deck";
-import React, {useContext, useEffect, useRef, useState} from "react";
+import React, {useEffect} from "react";
 import {Outlet, useNavigate} from "react-router";
-import {Link, NavLink} from "react-router-dom";
-import {setIcon} from "obsidian";
+import {NavLink} from "react-router-dom";
 import {ICON_LIST} from "src/constants";
 import {ModalContent} from "src/ui/views/modal";
 
@@ -48,43 +45,6 @@ export function Tabs() {
         </>
     );
 };
-
-// export function Notes({deck}:{deck: Deck}) {
-export function Notes() {
-    // TODO: rewrite to use props
-    const deck1 = {dueFlashcardsCount: 10, newFlashcardsCount:20, totalFlashcards: 30, deckName: "Deck1"} as Deck;
-    const deck2 = {dueFlashcardsCount: 40, newFlashcardsCount:40, totalFlashcards: 40, deckName: "Deck2"} as Deck;
-    const deckArray = [deck1, deck2];
-    const iconRef = useRef(null);
-
-    useEffect(() => {
-        const plus: Icon = 'plus-circle';
-        setIcon(iconRef.current, plus);
-    }, []);
-
-    // TODO: use onClick instead of Link?
-    return (
-        <div className={"Notes"}>
-           <ul className={"sr-deck-tree"}>
-               { deckArray.map((deck, i)=>(
-                   <Link to={'/notes/deck'}>
-                       <li className={"sr-deck tree-item-self is-clickable"} key={i}>
-                           <div className={"tree-item-inner"}>
-                               {deck.deckName}
-                           </div>
-                           <AllCardCounts deck={deck}/>
-                       </li>
-                   </Link>
-                   )
-               ) }
-           </ul>
-            <button className={"create-deck"}>
-                <div ref={iconRef}></div>
-                Add new deck
-            </button>
-        </div>
-    )
-}
 
 export function Tags() {
     return <ModalContent/>
