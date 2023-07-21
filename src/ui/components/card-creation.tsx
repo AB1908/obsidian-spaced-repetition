77d3ef80c87ff9552ed414763a918d5460fd6bf4
AB1908 <14124383+AB1908@users.noreply.github.com>
2023-07-21 16:39:42 +0530

    refactor(PreviewExistingFlashcards): move into routes

--- src/ui/components/card-creation.tsx
+++ src/ui/components/card-creation.tsx
@@ -1,52 +1,6 @@
 import React from "react";
 import {Form, Link} from "react-router-dom";
-import {useLoaderData} from "react-router";
-import {NoteAndHighlight} from "src/ui/components/note-and-highlight";
 import {CancelButton, SubmitButton} from "src/ui/components/buttons";
-import type {Annot} from "src/data/models/book";
-
-export async function highlightLoader({params}: {params: any}) {
-    // todo: use redirect
-    return fetch(`http://localhost:3000/flashcardsForAnnotation/${params.annotationId}`);
-}
-
-export function PreviewExistingFlashcards() {
-    const annotation = useLoaderData() as Annot;
-    return (
-        <>
-            <NoteAndHighlight highlightText={annotation.highlight} noteText={annotation.note}/>
-            <div>
-                {annotation.flashcards.length != 0 &&
-                    (<>
-                        <p>
-                            Existing questions:
-                        </p>
-                        <ul>
-                            {annotation.flashcards.map((t, i) => (
-                                <Link to={`${i}`}>
-                                    <li key={i}>
-                                        <p>
-                                            {t.questionText}
-                                        </p>
-                                        <p>
-                                            {t.answerText}
-                                        </p>
-                                    </li>
-                                </Link>
-                            ))}
-                        </ul>
-                    </>)
-                }
-                {/*TODO: This shouldn't be here I guess*/}
-                <Link to={"new"}>
-                    <button>
-                        Create New Cards
-                    </button>
-                </Link>
-            </div>
-        </>
-    )
-}
 
 export function TextInputWithLabel(props: { className: string, htmlFor: string, defaultValue: string }) {
     const labelText = props.htmlFor[0].toUpperCase() + props.htmlFor.slice(1);
