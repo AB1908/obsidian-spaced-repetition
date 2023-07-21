b94a20577611f40b6bd45429a18d4370da4b0377
AB1908 <14124383+AB1908@users.noreply.github.com>
2023-07-21 07:16:07 +0530

    refactor: remove click handler and use mock server in loader

--- src/ui/components/chapters-list.tsx
+++ src/ui/components/chapters-list.tsx
@@ -6,83 +6,15 @@ import {Tree} from "src/ui/components/tree";
 
 // TODO: more realistic data
 // TODO: think of a better data structure for this, this is terrible
-export function chapterLoader() {
-    return {
-        id: "alksdfj9",
-        name: "Book 1",
-        children: [
-            {
-                name: "Header 1",
-                id: "-g4c-q2S",
-                children: [
-                    {
-                        name: "SubHeader 1",
-                        id: "xHev-sAx",
-                        children: [],
-                    },
-                    {
-                        name: "SubHeader 2",
-                        id: "xHev-sA1",
-                        children: [],
-                    },
-                ],
-            },
-            {
-                name: "Header 2",
-                id: "eLy47ZoN",
-                children: [ ],
-            },
-            {
-                name: "Last header",
-                id: "WVcwnuIQ",
-                children: [
-                    {
-                        name: "Last subheader",
-                        id: "WVc23uIQ",
-                        children: [
-                        ],
-                    },
-                ],
-            },
-        ],
-        counts: {
-            "-g4c-q2S": {
-                "with": 1,
-                "without": 2,
-            },
-            "WVc23uIQ": {
-                "with": 0,
-                "without": 1,
-            },
-            "WVcwnuIQ": {
-                "with": 1,
-                "without": 1,
-            },
-            "alksdfj9": {
-                "with": 2,
-                "without": 4,
-            },
-            "eLy47ZoN": {
-                "with": 0,
-                "without": 1,
-            },
-            "xHev-sA1": {
-                "with": 0,
-                "without": 1,
-            },
-            "xHev-sAx": {
-                "with": 0,
-                "without": 1,
-            },
-        }
-    } as book;
+export function chapterLoader({params}: {params: any}) {
+    return fetch(`http://localhost:3000/books/${params.bookId}`);
 }
 
 // TODO: extract spans
 // TODO: add labels
 function Section({section, counts}: { section: any, counts: any }) {
-    const clickHandler = () => console.log("Clicked!");
-    return <div className="sr-deck tree-item-inner" onClick={clickHandler}>
+    // const clickHandler = () => console.log("Clicked!");
+    return <div className="sr-deck tree-item-inner" >
         {section.name}
         <span>
             {/*TODO: look into changing these class names? These ugly yo*/}
