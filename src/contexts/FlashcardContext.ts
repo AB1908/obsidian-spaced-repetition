c2ddfe9ffd79dde4961a702a8db39c0621bd7a7b
AB1908 <14124383+AB1908@users.noreply.github.com>
2023-07-23 04:29:50 +0530

    chore: fix wrong spelling

--- src/contexts/FlashcardContext.ts
+++ src/contexts/FlashcardContext.ts
@@ -1,11 +1,11 @@
 import React from "react";
 import {Card} from "src/Card";
 
-interface FlaschardContextProps{
+interface FlashcardContextProps{
     handleShowAnswerButton: Function;
     handleFlashcardResponse: Function;
     isQuestion: boolean;
     card: Card;
 }
 
-export const FlashcardContext = React.createContext<FlaschardContextProps>(undefined);
+export const FlashcardContext = React.createContext<FlashcardContextProps>(undefined);
