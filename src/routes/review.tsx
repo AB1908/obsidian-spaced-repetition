68ec751cc8bc2a6708c9e2c94a5f5933ced539b5
AB1908 <14124383+AB1908@users.noreply.github.com>
2023-07-23 06:06:06 +0530

    feat: use FlashcardContext

--- src/routes/review.tsx
+++ src/routes/review.tsx
@@ -2,6 +2,9 @@ import React, {useState} from "react";
 import {useLoaderData} from "react-router";
 import {Flashcard} from "src/data/models/flashcard";
 import {ResponseButtons, ShowAnswerButton} from "src/ui/components/buttons";
+import {FlashcardContext} from "src/contexts/FlashcardContext";
+import {ReviewResponse} from "src/scheduling";
+import {plugin, PluginData} from "src/main";
 
 export function reviewLoader({params}: {params: any}) {
     return fetch(`http://localhost:3000/review/${params.bookId}`)
@@ -41,11 +44,20 @@ export function ReviewDeck() {
     }
 
     return (<>
-        <Question questionText={currentCard.questionText}/>
-        <hr/>
-        <Answer answerText={currentCard.answerText}/>
-        <FlashcardFooter isQuestion={isQuestion} showAnswerHandler={() => setIsQuestion(false)}
+        <FlashcardContext.Provider
+            value={{
+                handleShowAnswerButton: () => setIsQuestion(false),
+                handleFlashcardResponse: (t: ReviewResponse) => handleResponseButtons(t),
+                isQuestion: isQuestion,
+                card: currentCard
+            }}
+        >
+            <Question questionText={currentCard.questionText}/>
+            <hr/>
+            <Answer answerText={currentCard.answerText}/>
+            <FlashcardFooter isQuestion={isQuestion} showAnswerHandler={() => setIsQuestion(false)}
                          card={currentCard}/>
+        </FlashcardContext.Provider>
     </>);
 }
 
