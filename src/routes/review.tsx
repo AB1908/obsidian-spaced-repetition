8cf7cfb126fb98fcb33d694e2a431c040068cdc8
AB1908 <14124383+AB1908@users.noreply.github.com>
2023-07-23 05:48:19 +0530

    feat: add new ReviewDeck component

--- src/routes/review.tsx
+++ src/routes/review.tsx
@@ -1,1 +1,41 @@
-import React from "react";
+import React, {useState} from "react";
+import {useLoaderData} from "react-router";
+import {Flashcard} from "src/data/models/flashcard";
+import {ResponseButtons, ShowAnswerButton} from "src/ui/components/buttons";
+
+export function reviewLoader({params}: {params: any}) {
+    return fetch(`http://localhost:3000/review/${params.bookId}`)
+}
+
+function Question(props: { questionText: string }) {
+    return <p>
+        {props.questionText}
+    </p>;
+}
+
+function Answer(props: { answerText: string }) {
+    return <p>
+        {props.answerText}
+    </p>;
+}
+
+export function ReviewDeck() {
+    const flashcards: Flashcard[] = (useLoaderData() as any).flashcards;
+    const [flashcardIndex, setFlashcardIndex] = useState(0);
+    const [isQuestion, setIsQuestion] = useState(true);
+    const currentCard = flashcards[flashcardIndex];
+    return (<>
+        <Question questionText={currentCard.questionText}/>
+        <hr/>
+        <Answer answerText={currentCard.answerText}/>
+        <FlashcardFooter isQuestion={isQuestion} showAnswerHandler={() => setIsQuestion(false)}
+                         card={currentCard}/>
+    </>);
+}
+
+function FlashcardFooter({isQuestion, showAnswerHandler, card}: {isQuestion: boolean, showAnswerHandler: () => void, card: Flashcard}) {
+    if (isQuestion)
+        return <ShowAnswerButton handleShowAnswerButton={() => showAnswerHandler()}/>;
+    else
+        return <ResponseButtons card={card} handleFlashcardResponse={(response: any) => console.log("Clicked!")}/>;
+}
