255053d3ed0cb5e6b251618e903d3e854ec544ef
AB1908 <14124383+AB1908@users.noreply.github.com>
2023-07-23 06:04:41 +0530

    feat: widen type for FlashcardContext

--- src/contexts/FlashcardContext.ts
+++ src/contexts/FlashcardContext.ts
@@ -1,11 +1,12 @@
 import React from "react";
 import {Card} from "src/Card";
+import {Flashcard} from "src/data/models/flashcard";
 
 interface FlashcardContextProps{
     handleShowAnswerButton: Function;
     handleFlashcardResponse: Function;
     isQuestion: boolean;
-    card: Card;
+    card: Card|Flashcard;
 }
 
 export const FlashcardContext = React.createContext<FlashcardContextProps>(undefined);
