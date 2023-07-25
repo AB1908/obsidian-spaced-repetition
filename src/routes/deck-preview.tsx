60f89e39eb89c3e3a9c83eab0e01ff9ac697355b
AB1908 <14124383+AB1908@users.noreply.github.com>
2023-07-25 16:25:59 +0530

    feat: add method to count annotation flashcard coverage

--- src/routes/deck-preview.tsx
+++ src/routes/deck-preview.tsx
@@ -34,3 +34,15 @@ export function DeckLandingPage() {
         </>
     )
 }
+
+function countTotals(book: book) {
+    let countWith: number = 0;
+    let countWithout: number = 0;
+    for (let sectionKey of Object.keys(book.counts.sections)) {
+        //@ts-ignore
+        const item: Count = book.counts.sections[sectionKey];
+        countWith += item.with;
+        countWithout += item.without;
+    }
+    return {countWith, countWithout};
+}
