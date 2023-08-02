efd3b4f6edfe473d5489e3a2f5f9df0ed4806995
AB1908 <14124383+AB1908@users.noreply.github.com>
2023-08-02 08:28:57 +0530

    refactor: move generateFlashcardsArray into flashcards.ts

--- src/data/parser.ts
+++ src/data/parser.ts
@@ -173,11 +173,3 @@ export function parseMetadata(text: string): FlashcardMetadata {
         };
 }
 
-export function generateFlashcardsArray(parsedCardsArray: ParsedCard[]) {
-    const out: Flashcard[] = [];
-    for (let parsedCard of parsedCardsArray) {
-        const [questionText, answerText] = parseCardText(parsedCard.cardText);
-        out.push(new Flashcard(parsedCard.id, questionText, answerText, parseMetadata(parsedCard.metadataText)));
-    }
-    return out;
-}
