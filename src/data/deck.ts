f94b8448d0e9c896b17fe8a17c75d3fc960ec1b8
AB1908 <14124383+AB1908@users.noreply.github.com>
2023-08-04 18:00:02 +0530

    refactor: move maturityCount into flashcard model

--- src/data/deck.ts
+++ src/data/deck.ts
@@ -1,23 +1,8 @@
 import {parse} from "src/old_parser";
 import {SRSettings} from "src/settings";
-import {Flashcard} from "src/data/models/flashcard";
 import {plugin} from "src/main";
 
 export function createParsedCardsArray(fileText: string, settings: SRSettings) {
     plugin.parsedCards = [...parse(fileText, settings)]
 }
 
-export function maturityCounts(flashcards: Flashcard[])  {
-    let newCount = 0, mature = 0, learning = 0;
-    flashcards.forEach(t => {
-        if (t.interval >= 32) {
-            mature += 1;
-        } else if (t.interval && t.interval < 32) {
-            learning += 1;
-        } else {
-        // } if (!t.interval) {
-            newCount += 1;
-        }
-    });
-    return {mature, learning, new: newCount};
-}
