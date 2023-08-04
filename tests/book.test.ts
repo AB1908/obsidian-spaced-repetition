cc4745e3f22f512606ed46f570dc104b38959141
AB1908 <14124383+AB1908@users.noreply.github.com>
2023-08-04 17:53:34 +0530

    refactor: move findPreviousHeader change to book tests

--- tests/book.test.ts
+++ tests/book.test.ts
@@ -1,12 +1,22 @@
-import {AnnotationCount, bookSections, generateTree, getAnnotationsForSection, Heading} from "src/data/models/book";
+import {
+    AnnotationCount,
+    bookSections,
+    findPreviousHeader,
+    generateTree,
+    getAnnotationsForSection,
+    Heading
+} from "src/data/models/book";
 import {sampleAnnotationMetadata, sampleAnnotationText} from "./disk.test";
 import {annotation} from "src/data/import/annotations";
 import {bookWithCounts} from "src/api";
+import {beforeEach} from "@jest/globals";
+import {SectionCache} from "obsidian";
 
 const { nanoid } = jest.requireActual("nanoid");
 jest.doMock("nanoid", () => ({
     nanoid: nanoid,
 }));
+jest.mock("../src/main", () => {});
 
 let bookSectionsArray = [
     {
@@ -385,5 +395,57 @@ describe("getAnnotations", () => {
                 type: "notes",
             },
         ]);
+    });
+});
+
+let input: any[];
+describe("findPreviousHeader", () => {
+    beforeEach(() => {
+        input = [
+            {
+                heading: "Heading 1",
+                level: 1,
+            },
+            {
+                type: "paragraph",
+            },
+            {
+                heading: "SubHeading 1",
+                level: 2,
+            },
+            {
+                type: "paragraph",
+            },
+            {
+                heading: "SubHeading 2",
+                level: 2,
+            },
+            {
+                type: "paragraph",
+            },
+            {
+                heading: "Heading 2",
+                level: 1,
+            },
+            {
+                type: "paragraph",
+            },
+        ];
+    });
+
+    test("should return null for a top level header", () => {
+        expect(findPreviousHeader(input as SectionCache[], input[6] as SectionCache)).toBe(null);
+        expect(findPreviousHeader(input as SectionCache[], input[0] as SectionCache)).toBe(null);
+    });
+    test("should return a top level header for a subheader", () => {
+        expect(findPreviousHeader(input as SectionCache[], input[4] as SectionCache)).toBe(0);
+        expect(findPreviousHeader(input as SectionCache[], input[2] as SectionCache)).toBe(0);
+    });
+    // TODO: test for subsubheaders :(
+    test("should return a top level header for an annotation under it", () => {
+        expect(findPreviousHeader(input as SectionCache[], input[1] as SectionCache)).toBe(0);
+    });
+    test("should return a top level header for an annotation under it", () => {
+        expect(findPreviousHeader(input as SectionCache[], input[3] as SectionCache)).toBe(2);
     });
 });
