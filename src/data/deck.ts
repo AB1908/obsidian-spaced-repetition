5e23cca29dcc06f1c4b5087a277bac83533143c0
AB1908 <14124383+AB1908@users.noreply.github.com>
2023-08-06 14:13:57 +0530

    chore: remove unused

--- src/data/deck.ts
+++ src/data/deck.ts
@@ -1,8 +1,3 @@
 import {parse} from "src/old_parser";
 import {SRSettings} from "src/settings";
 import {plugin} from "src/main";
-
-export function createParsedCardsArray(fileText: string, settings: SRSettings) {
-    plugin.parsedCards = [...parse(fileText, settings)]
-}
-
