7d93cf031b67211cb195edfececbbec5305dd480
AB1908 <14124383+AB1908@users.noreply.github.com>
2023-08-09 21:51:21 +0530

    refactor: add factory method for creating Flashcard objects

--- src/data/import/flashcards.ts
+++ src/data/import/flashcards.ts
@@ -2,11 +2,15 @@ import {ParsedCard} from "src/data/models/parsedCard";
 import {Flashcard} from "src/data/models/flashcard";
 import {parseCardText, parseMetadata} from "src/data/parser";
 
+export function createFlashcard(parsedCard: ParsedCard, questionText: string, answerText: string) {
+    return new Flashcard(parsedCard.id, questionText, answerText, parseMetadata(parsedCard.metadataText));
+}
+
 export function generateFlashcardsArray(parsedCardsArray: ParsedCard[]) {
     const out: Flashcard[] = [];
     for (let parsedCard of parsedCardsArray) {
         const [questionText, answerText] = parseCardText(parsedCard.cardText);
-        out.push(new Flashcard(parsedCard.id, questionText, answerText, parseMetadata(parsedCard.metadataText)));
+        out.push(createFlashcard(parsedCard, questionText, answerText));
     }
     return out;
 }
