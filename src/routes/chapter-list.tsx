d042e1ec8d1d4539fb6de257c1ea44afcd63d355
AB1908 <14124383+AB1908@users.noreply.github.com>
2023-08-13 12:30:07 +0530

    refactor: move Section to its own component file

--- src/routes/chapter-list.tsx
+++ src/routes/chapter-list.tsx
@@ -17,30 +17,6 @@ export function chapterLoader({params}: {params: any}) {
     }
 }
 
-// TODO: extract spans
-// TODO: add labels
-function Section({section, counts}: { section: book, counts: Counts }) {
-    // const clickHandler = () => console.log("Clicked!");
-    const sectionId: string = section.id;
-    return <Link to={`${sectionId}/annotations`}>
-        <div className="sr-deck tree-item-inner">
-        {section.name}
-            <span>
-                {/*TODO: look into changing these class names? These ugly yo*/}
-                <span className={"yes-tests tree-item-flair sr-test-counts"}>
-                    {/*// TODO: potential for this to be null since spec for flashcard array not defined yet*/}
-                    {section.count.with}
-                </span>
-                <span className={"no-tests tree-item-flair sr-test-counts"}>
-                    {/*// TODO: potential for this to be null since spec for flashcard array not defined yet*/}
-                    {/*{section.with}*/}
-                    {section.count.without}
-                </span>
-            </span>
-        </div>
-    </Link>;
-}
-
 // TODO: Fix counts for header
 // TODO: Fix state changes when clicking?
 // TODO: Actually allow clicking?
