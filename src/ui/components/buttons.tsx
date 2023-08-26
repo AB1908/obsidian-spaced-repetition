3f457f998e022c8b1017dd8c8bc854c418c6b21e
AB1908 <14124383+AB1908@users.noreply.github.com>
2023-08-26 18:55:31 +0530

    refactor: move button text generator to button component file

--- src/ui/components/buttons.tsx
+++ src/ui/components/buttons.tsx
@@ -1,6 +1,8 @@
 import React from "react";
-import {ReviewResponse} from "src/scheduler/scheduling";
+import {ReviewResponse, textInterval} from "src/scheduler/scheduling";
 import {Link} from "react-router-dom";
+import {Platform} from "obsidian";
+import {plugin} from "src/main";
 
 export function ShowAnswerButton(props: {handleShowAnswerButton: Function}) {
     return (
@@ -32,4 +34,20 @@ export function CancelButton() {
 
 export function SubmitButton() {
     return <button type="submit" className={"mod-cta"}>Submit</button>;
+}
+
+export function generateButtonText(hardInterval: number, goodInterval: number, easyInterval: number) {
+    let hardBtnText, goodBtnText, easyBtnText;
+    if (Platform.isMobile) {
+        hardBtnText = `${textInterval(hardInterval, true)}`;
+        goodBtnText = `${textInterval(goodInterval, true)}`;
+        easyBtnText = `${textInterval(easyInterval, true)}`;
+    } else {
+        // TODO: investigate fix for button labels being empty
+        const {data} = plugin;
+        hardBtnText = `${data.settings.flashcardHardText} - ${textInterval(hardInterval, false)}`
+        goodBtnText = `${data.settings.flashcardGoodText} - ${textInterval(goodInterval, false)}`;
+        easyBtnText = `${data.settings.flashcardEasyText} - ${textInterval(easyInterval, false)}`;
+    }
+    return {hardBtnText, goodBtnText, easyBtnText};
 }
