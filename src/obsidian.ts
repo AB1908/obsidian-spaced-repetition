df05fe3a3f91239b1640f1115ce18b0da47f0fc6
AB1908 <14124383+AB1908@users.noreply.github.com>
2023-08-29 16:30:59 +0530

    fix: extend obsidian API definition to include internal APIs

--- src/obsidian.ts
+++ src/obsidian.ts
@@ -1,7 +1,18 @@
-import type {} from "obsidian";
+import type {CachedMetadata} from "obsidian";
 
 declare module "obsidian" {
     interface HeadingCache {
         display: string
+    }
+
+    interface MetadataCache {
+        metadataCache: Record<string, CachedMetadata>;
+        fileCache: Record<string, InternalFileInfo>;
+    }
+
+    interface InternalFileInfo {
+        mtime: number;
+        hash: string;
+        size: number;
     }
 }
