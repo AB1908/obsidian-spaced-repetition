968f2265811599bc3371cac66ab12c33ef2995b4
AB1908 <14124383+AB1908@users.noreply.github.com>
2023-08-29 16:30:35 +0530

    chore: add todo note

--- src/routes/errorPage.tsx
+++ src/routes/errorPage.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { useRouteError } from "react-router-dom";
 
 export default function ErrorPage() {
+    // todo: what is the correct type for this?
     const error = useRouteError();
     console.error(error);
 
