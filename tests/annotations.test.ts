04d1643dd25e7b28ff132092a81a17825bdca9b7
AB1908 <14124383+AB1908@users.noreply.github.com>
2023-08-31 20:47:45 +0530

    test: fix tests and formatting

--- tests/annotations.test.ts
+++ tests/annotations.test.ts
@@ -1,107 +1,138 @@
-import {parseAnnotations} from "src/data/import/annotations";
+import { parseAnnotations } from "src/data/import/annotations";
 
 describe("parseAnnotations should parse an annotation with", () => {
     test("a single line highlight", () => {
-        expect(parseAnnotations(`# Heading 1
+        expect(
+            parseAnnotations(`# Heading 1
 
 > [!notes] 93813
 > Onen i estel Edain, u-chebin estel anim.
 > ***
 > 
-`)).toEqual({
-            id: 93813,
-            type: "notes",
-            highlight: "> Onen i estel Edain, u-chebin estel anim.",
-            note: ">"
-        });
+`)
+        ).toMatchInlineSnapshot(`
+            {
+              "highlight": "Onen i estel Edain, u-chebin estel anim.",
+              "id": "93813",
+              "note": "",
+              "type": "notes",
+            }
+        `);
     });
 
     test("a single line note and single line highlight", () => {
-        expect(parseAnnotations(`# Heading 1
+        expect(
+            parseAnnotations(`# Heading 1
 
 > [!notes] 93813
 > Onen i estel Edain, u-chebin estel anim.
 > ***
 > What a beautiful line by Tolkien
-`)).toEqual({
-            id: 93813,
-            type: "notes",
-            highlight: "> Onen i estel Edain, u-chebin estel anim.",
-            note: "> What a beautiful line by Tolkien"
-        });
-        expect(parseAnnotations(`# Heading 1
+`)
+        ).toMatchInlineSnapshot(`
+            {
+              "highlight": "Onen i estel Edain, u-chebin estel anim.",
+              "id": "93813",
+              "note": "What a beautiful line by Tolkien",
+              "type": "notes",
+            }
+        `);
+        expect(
+            parseAnnotations(`# Heading 1
 
 > [!notes] 93813
 > Onen i estel Edain, u-chebin estel anim.
 > ***
-> What a beautiful line by Tolkien`)).toEqual({
-            id: 93813,
-            type: "notes",
-            highlight: "> Onen i estel Edain, u-chebin estel anim.",
-            note: "> What a beautiful line by Tolkien"
-        });
+> What a beautiful line by Tolkien`)
+        ).toMatchInlineSnapshot(`
+            {
+              "highlight": "Onen i estel Edain, u-chebin estel anim.",
+              "id": "93813",
+              "note": "What a beautiful line by Tolkien",
+              "type": "notes",
+            }
+        `);
     });
 
     test("a single line note and multi line highlight", () => {
-        expect(parseAnnotations(`# Heading 1
+        expect(
+            parseAnnotations(`# Heading 1
 
 > [!notes] 93813
 > Onen i estel Edain, u-chebin estel anim.
 > This is another line.
 > ***
-> What a beautiful line by Tolkien`)).toEqual({
-            id: 93813,
-            type: "notes",
-            highlight: "> Onen i estel Edain, u-chebin estel anim.\n> This is another line.",
-            note: "> What a beautiful line by Tolkien"
-        });
-        expect(parseAnnotations(`# Heading 1
+> What a beautiful line by Tolkien`)
+        ).toMatchInlineSnapshot(`
+            {
+              "highlight": "Onen i estel Edain, u-chebin estel anim.
+            This is another line.",
+              "id": "93813",
+              "note": "What a beautiful line by Tolkien",
+              "type": "notes",
+            }
+        `);
+        expect(
+            parseAnnotations(`# Heading 1
 
 > [!notes] 93813
 > Onen i estel Edain, u-chebin estel anim.
 > This is another line.
 > ***
 > What a beautiful line by Tolkien
-`)).toEqual({
-            id: 93813,
-            type: "notes",
-            highlight: "> Onen i estel Edain, u-chebin estel anim.\n> This is another line.",
-            note: "> What a beautiful line by Tolkien"
-        });
+`)
+        ).toMatchInlineSnapshot(`
+            {
+              "highlight": "Onen i estel Edain, u-chebin estel anim.
+            This is another line.",
+              "id": "93813",
+              "note": "What a beautiful line by Tolkien",
+              "type": "notes",
+            }
+        `);
     });
 
     test("a multi line note and single line highlight", () => {
-
-        expect(parseAnnotations(`# Heading 1
+        expect(
+            parseAnnotations(`# Heading 1
 
 > [!notes] 93813
 > Onen i estel Edain, u-chebin estel anim.
 > ***
 > What a beautiful line by Tolkien
 > This is another line.
-`)).toEqual({
-            id: 93813,
-            type: "notes",
-            highlight: "> Onen i estel Edain, u-chebin estel anim.",
-            note: "> What a beautiful line by Tolkien\n> This is another line."
-        });
-        expect(parseAnnotations(`# Heading 1
+`)
+        ).toMatchInlineSnapshot(`
+            {
+              "highlight": "Onen i estel Edain, u-chebin estel anim.",
+              "id": "93813",
+              "note": "What a beautiful line by Tolkien
+            This is another line.",
+              "type": "notes",
+            }
+        `);
+        expect(
+            parseAnnotations(`# Heading 1
 
 > [!notes] 93813
 > Onen i estel Edain, u-chebin estel anim.
 > ***
 > What a beautiful line by Tolkien
-> This is another line.`)).toEqual({
-            id: 93813,
-            type: "notes",
-            highlight: "> Onen i estel Edain, u-chebin estel anim.",
-            note: "> What a beautiful line by Tolkien\n> This is another line."
-        });
-
+> This is another line.`)
+        ).toMatchInlineSnapshot(`
+            {
+              "highlight": "Onen i estel Edain, u-chebin estel anim.",
+              "id": "93813",
+              "note": "What a beautiful line by Tolkien
+            This is another line.",
+              "type": "notes",
+            }
+        `);
     });
 
     test("a multi line note and multi line highlight", () => {
-        expect(parseAnnotations(`# Heading 1
+        expect(
+            parseAnnotations(`# Heading 1
 
 > [!notes] 93813
 > Onen i estel Edain, u-chebin estel anim.
@@ -109,24 +140,35 @@ describe("parseAnnotations should parse an annotation with", () => {
 > ***
 > What a beautiful line by Tolkien
 > This is another line.
-`)).toEqual({
-            id: 93813,
-            type: "notes",
-            highlight: "> Onen i estel Edain, u-chebin estel anim.\n> This is another line.",
-            note: "> What a beautiful line by Tolkien\n> This is another line."
-        });
-        expect(parseAnnotations(`# Heading 1
+`)
+        ).toMatchInlineSnapshot(`
+            {
+              "highlight": "Onen i estel Edain, u-chebin estel anim.
+            This is another line.",
+              "id": "93813",
+              "note": "What a beautiful line by Tolkien
+            This is another line.",
+              "type": "notes",
+            }
+        `);
+        expect(
+            parseAnnotations(`# Heading 1
 
 > [!notes] 93813
 > Onen i estel Edain, u-chebin estel anim.
 > This is another line.
 > ***
 > What a beautiful line by Tolkien
-> This is another line.`)).toEqual({
-            id: 93813,
-            type: "notes",
-            highlight: "> Onen i estel Edain, u-chebin estel anim.\n> This is another line.",
-            note: "> What a beautiful line by Tolkien\n> This is another line."
-        });
+> This is another line.`)
+        ).toMatchInlineSnapshot(`
+            {
+              "highlight": "Onen i estel Edain, u-chebin estel anim.
+            This is another line.",
+              "id": "93813",
+              "note": "What a beautiful line by Tolkien
+            This is another line.",
+              "type": "notes",
+            }
+        `);
     });
 });
