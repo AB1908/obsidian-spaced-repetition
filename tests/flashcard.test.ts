ea4a188a934756e8722a6b62f21574c95835c420
AB1908 <14124383+AB1908@users.noreply.github.com>
2023-09-02 08:08:10 +0530

    test: add simple creation test

---
+++ tests/flashcard.test.ts
@@ -0,0 +1,38 @@
+import { CardType } from "src/scheduler/scheduling";
+import { generateFlashcardsArray } from "src/data/import/flashcards";
+
+// eslint-disable-next-line @typescript-eslint/no-empty-function
+jest.mock("../src/main", () => {});
+
+describe("createFlashcards", () => {
+    test("should create a flashcard array", () => {
+        const parsedCards = [
+            {
+                id: "aaaaaaaa",
+                notePath: "sample path",
+                cardText: "This is a question\n?\nThis is an answer",
+                metadataText: "<!--SR:93813-->",
+                lineNo: -1,
+                cardType: CardType.MultiLineBasic,
+            },
+        ];
+        expect(generateFlashcardsArray(parsedCards)).toMatchInlineSnapshot(`
+            [
+              Flashcard {
+                "annotationId": "93813",
+                "answerText": "This is an answer",
+                "cardType": 2,
+                "context": null,
+                "dueDate": null,
+                "ease": null,
+                "flag": null,
+                "id": 0,
+                "interval": null,
+                "parsedCardId": "aaaaaaaa",
+                "questionText": "This is a question",
+                "siblings": [],
+              },
+            ]
+        `);
+    });
+});
