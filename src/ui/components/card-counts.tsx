b845350bf1f756de25b99394919324055fc6c695
AB1908 <14124383+AB1908@users.noreply.github.com>
2023-09-03 18:28:37 +0530

    refactor: rewrite CardCount to be a functional component

--- src/ui/components/card-counts.tsx
+++ src/ui/components/card-counts.tsx
@@ -1,23 +1,12 @@
 import React from "react";
-import { t } from "src/lang/helpers";
 
-// FIX: types
-interface CardCountProps {
-    cardType: any;
-    backgroundColor?: string;
-    cardCount: number;
-    additionalClass: string;
-}
-
-export class CardCount extends React.Component<CardCountProps> {
-    render(): React.ReactNode {
-        return (
-            <span
-                aria-label={t(this.props.cardType)}
-                className={`tree-item-flair sr-deck-counts ${this.props.additionalClass}`}
-            >
-                {this.props.cardCount.toString()}
+export function CardCount({cardType, additionalClass, cardCount}: {cardType: string, additionalClass: string, cardCount: number}) {
+    return (
+        <span
+            aria-label={cardType}
+            className={`tree-item-flair sr-deck-counts ${additionalClass}`}
+        >
+                {cardCount.toString()}
             </span>
-        );
-    }
+    );
 }
