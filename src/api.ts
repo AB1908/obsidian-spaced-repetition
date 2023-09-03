045bc73bb48a6ce4e93b14f4451e82929151a5d2
AB1908 <14124383+AB1908@users.noreply.github.com>
2023-09-04 02:55:18 +0530

    refactor: remove unused methods

--- src/api.ts
+++ src/api.ts
@@ -78,16 +78,6 @@ export function getFlashcardById(flashcardId: string, bookId: string): FrontendF
     return flashcard;
 }
 
-// TODO: add logic to update in storage
-export function updateFlashcardQuestion(id: string, question: string) {
-    const card = plugin.flashcards.filter((t: AbstractFlashcard) => t.id === id)[0];
-    if (card === undefined) {
-        return false;
-    }
-    card.questionText = question;
-    return true;
-}
-
 export async function updateFlashcardSchedulingMetadata(
     flashcardId: string,
     bookId: string,
@@ -102,17 +92,7 @@ export async function updateFlashcardSchedulingMetadata(
     return true;
 }
 
-// TODO: add logic to update in storage
-export function updateFlashcardAnswer(id: string, answer: string) {
-    const card = plugin.flashcards.filter((t: AbstractFlashcard) => t.id === id)[0];
-    if (card === undefined) {
-        return false;
-    }
-    // TODO: add logic to update parsed card as well
-    card.answerText = answer;
-    return true;
-}
-
+// DONE: add logic to update in storage
 export async function createFlashcardForAnnotation(question: string, answer: string, annotationId: string, bookId: string, cardType: CardType = CardType.MultiLineBasic) {
     let card;
     const book = plugin.notesWithFlashcards.filter(t => t.id === bookId)[0];
@@ -162,18 +142,6 @@ export async function updateFlashcardContentsById(flashcardId: string, question:
             throw new Error("not implemented");
         }
     }
-    return true;
-}
-
-// TODO: add logic to remove from storage
-export function deleteFlashcardById(id: string) {
-    if (plugin.flashcards.length == 0) {
-        throw new Error("Array of flashcards is empty!");
-    }
-    if (plugin.flashcards.findIndex((f: AbstractFlashcard) => f.id === id) == -1) {
-        return false;
-    }
-    plugin.flashcards = plugin.flashcards.filter((f: AbstractFlashcard) => f.id !== id);
     return true;
 }
 
