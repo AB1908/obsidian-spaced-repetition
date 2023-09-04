20a6c4982faddc80773460aec8a21c121d0ae37c
AB1908 <14124383+AB1908@users.noreply.github.com>
2023-09-05 04:00:24 +0530

    test: update generateFlashcards array test snapshot to have correct type for attribute

--- tests/flashcard.test.ts
+++ tests/flashcard.test.ts
@@ -2,7 +2,8 @@ import { CardType } from "src/scheduler/scheduling";
 import { generateFlashcardsArray } from "src/data/import/flashcards";
 
 // eslint-disable-next-line @typescript-eslint/no-empty-function
-jest.mock("../src/main", () => {});
+jest.mock("../src/main", () => {
+});
 
 describe("createFlashcards", () => {
     test("should create a flashcard array", () => {
@@ -13,8 +14,8 @@ describe("createFlashcards", () => {
                 cardText: "This is a question\n?\nThis is an answer",
                 metadataText: "<!--SR:93813-->",
                 lineNo: -1,
-                cardType: CardType.MultiLineBasic,
-            },
+                cardType: CardType.MultiLineBasic
+            }
         ];
         expect(generateFlashcardsArray(parsedCards)).toMatchInlineSnapshot(`
             [
@@ -26,7 +27,7 @@ describe("createFlashcards", () => {
                 "dueDate": null,
                 "ease": null,
                 "flag": null,
-                "id": 0,
+                "id": "0",
                 "interval": null,
                 "parsedCardId": "aaaaaaaa",
                 "questionText": "This is a question",
