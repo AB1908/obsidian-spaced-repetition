AB1908/obsidian-spaced-repetition

commit b9c09d2e46bb00e9607af3e52fedfdaa09ea90fa

chore: remove location logging

src/routes/review.tsx
@@ -1,5 +1,5 @@
 import React, {useEffect, useRef, useState} from "react";
-import {redirect, useLoaderData, useLocation, useNavigate, useParams} from "react-router-dom";
+import {redirect, useLoaderData, useNavigate, useParams} from "react-router-dom";
 import {getCurrentCard, getFlashcardById, getNextCard, updateFlashcardSchedulingMetadata} from "src/api";
 import {CardBack, CardFront} from "src/ui/components/flashcard";
 import {setIcon} from "obsidian";
@@ -55,11 +55,9 @@ export async function reviewLoader({params}: {params: ReviewLoaderParams}) {
 export function ReviewDeck() {
     const currentCard = useLoaderData() as FrontendFlashcard;
     const [isQuestion, setIsQuestion] = useState(true);
-    const location = useLocation();
     const params = useParams<keyof ReviewLoaderParams>();
     const navigate = useNavigate();
     const editButton = useRef<HTMLDivElement>(null);
-    console.log(useLocation().pathname)
 
     useEffect(() => {
         const editIcon: Icon = "lucide-pencil";

commit d1e2fc0ae612acc883255160bd175b1d51e91bb4

fix: location state breakage in b9c09d2
- It's odd this wasn't caught by my linter at the time.

src/routes/review.tsx
@@ -1,5 +1,5 @@
 import React, {useEffect, useRef, useState} from "react";
-import {redirect, useLoaderData, useNavigate, useParams} from "react-router-dom";
+import {redirect, useLoaderData, useLocation, useNavigate, useParams} from "react-router-dom";
 import {getCurrentCard, getFlashcardById, getNextCard, updateFlashcardSchedulingMetadata} from "src/api";
 import {CardBack, CardFront} from "src/ui/components/flashcard";
 import {setIcon} from "obsidian";
@@ -58,6 +58,7 @@ export function ReviewDeck() {
     const params = useParams<keyof ReviewLoaderParams>();
     const navigate = useNavigate();
     const editButton = useRef<HTMLDivElement>(null);
+    const location = useLocation();
 
     useEffect(() => {
         const editIcon: Icon = "lucide-pencil";