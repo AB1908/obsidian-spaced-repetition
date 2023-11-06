7e60b058185d39efa881d8248eb0c78bd1fb9587
AB1908 <14124383+AB1908@users.noreply.github.com>
2023-11-06 19:35:40 +0530

    feat: add wrapper div around annotation and note blockquotes

--- src/ui/components/note-and-highlight.tsx
+++ src/ui/components/note-and-highlight.tsx
@@ -2,20 +2,19 @@ import React from "react";
 
 export function NoteAndHighlight({highlightText, noteText}: { highlightText: string, noteText: string }) {
     return <>
-        <div className={"sr-annotation"}>
-            <blockquote className={"sr-blockquote"}>
+        <div className={"annotation-note"}>
+            <blockquote className={"sr-blockquote-annotation"}>
                 <p>
                     {highlightText}
                 </p>
             </blockquote>
-
+            {noteText &&
+                <blockquote className={"sr-blockquote-note"}>
+                    <p>
+                        {noteText}
+                    </p>
+                </blockquote>
+            }
         </div>
-        {noteText && <div>
-            <blockquote className={"sr-blockquote"}>
-                <p>
-                    {noteText}
-                </p>
-            </blockquote>
-        </div>}
     </>;
 }
