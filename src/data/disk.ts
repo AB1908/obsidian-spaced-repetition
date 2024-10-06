3fce99d37b8af0721d2ae797ddd04766100150dd
AB1908 <14124383+AB1908@users.noreply.github.com>
2024-10-06 13:30:43 -0700

    chore: add type annotation

--- src/data/disk.ts
+++ src/data/disk.ts
@@ -79,7 +79,7 @@ export function fileTags() {
     return tagMap;
 }
 
-export function filePathsWithTag(tag: string) {
+export function filePathsWithTag(tag: string): string[] {
     const hashSet = setOfHashesWithTags(tag);
     return findFilesWithHashInSet(hashSet);
 }
