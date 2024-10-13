cc1e5dc6ccda4c57b286fb1f851471a36c68e6e1
AB1908 <14124383+AB1908@users.noreply.github.com>
2024-10-13 12:56:29 -0700

    test: add and fix mock implementations

--- src/data/__mocks__/disk.ts
+++ src/data/__mocks__/disk.ts
@@ -1,22 +1,15 @@
 import { readdirSync, readFileSync } from "fs";
-import { getTFileForPath } from "src/data/disk";
-import { TFile } from "obsidian";
-
-// const disk = jest.genMockFromModule("src/data/disk");
 
 const testdir = "/Users/abhishek/Documents/GitHub/obsidian-spaced-repetition/tests/json";
 const contents = readdirSync(testdir);
 const inputFileList = contents.filter(t => t.includes("input"));
 const inputContents = inputFileList.map(t => JSON.parse(readFileSync(`${testdir}/${t}`).toString("utf8")));
 
-const writeCardToDisk = (path: string, text: string) => {
-};
-
 export function filePathsWithTag(tag: string): string[] {
     return [ "Atomic Habits/Flashcards.md", "Memory - A Very Short Introduction/Flashcards.md", "Untitled - Flashcards.md"];
 }
 
-const getParentOrFilename = (path: string) => {
+export const getParentOrFilename = (path: string) => {
     if (path == "Atomic Habits/Annotations.md")
         return "Atomic Habits";
     if (path == "Memory - A Very Short Introduction/Annotations.md")
@@ -39,11 +32,73 @@ export function getMetadataForFile(path: string) {
 }
 
 export function getFileFromLinkText(annotationLinkText: string, path: string) {
-    const outputFileName = inputContents.filter(t=> t.path == path && t.fileName && t.fileName.includes("getFileFromLinkText"))[0].fileName;
-    return JSON.parse(outputLoader(outputFileName));
+    const outputFileName = inputContents.filter(t=> t.fileName.includes("getFileFromLinkText") && t.annotationLinkText == annotationLinkText)[0].fileName;
+    return JSON.parse(outputLoader(outputFileName))["obj"];
 }
 
 export async function getFileContents(path: string) {
     const outputFileName = inputContents.filter(t=> t.path == path && t.fileName && t.fileName.includes("getFileContents"))[0].fileName;
     return JSON.parse(outputLoader(outputFileName))["obj"];
+}
+
+export async function writeCardToDisk(path: string, text: string) {
+    return jest.fn(() => {return {path ,text};});
+    // const outputFileName = inputContents.filter(t=> t.path == path && t.fileName && t.fileName.includes("getFileContents"))[0].fileName;
+    // return;
+}
+
+export function fileTags() {
+    // @ts-ignore
+    return new Map([
+        [
+            "Atomic Habits/Annotations.md",
+            [
+                "review/book",
+                "research"
+            ]
+        ],
+        [
+            "Atomic Habits/Flashcards.md",
+            [
+                "flashcards"
+            ]
+        ],
+        [
+            "transactions.ledger",
+            []
+        ],
+        [
+            "Learning/Learning.md",
+            "review/book"
+        ],
+        [
+            "Memory - A Very Short Introduction/Annotations.md",
+            [
+                "review/book"
+            ]
+        ],
+        [
+            "Memory - A Very Short Introduction/Flashcards.md",
+            [
+                "flashcards"
+            ]
+        ],
+        [
+            "Untitled - Flashcards.md",
+            [
+                "flashcards"
+            ]
+        ],
+        [
+            "Untitled.md",
+            [
+                "review/book",
+                "review/note"
+            ]
+        ]
+    ]);
+}
+
+export function getCurrentDateIsoString() {
+    return "2024-10-10";
 }
